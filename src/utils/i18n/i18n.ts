77a950ba1f20871d5808ab8f2edb6cd513f9efab
ariser <n.trshkv@gmail.com>
2022-05-25 14:54:42 +0300

    chore: update @yandex-cloud/i18n to v0.6.0

--- src/utils/i18n/i18n.ts
+++ src/utils/i18n/i18n.ts
@@ -1,7 +1,8 @@
 import {I18N} from '@yandex-cloud/i18n';
 
-export type Lang = keyof typeof I18N.LANGS;
+export enum Lang {
+    En = 'en',
+    Ru = 'ru',
+}
 
 export const i18n = new I18N();
-
-export {I18N} from '@yandex-cloud/i18n';
