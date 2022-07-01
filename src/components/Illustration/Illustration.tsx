7d10880cd4d9f945e7c8a7232327d8db68f0865c
ariser <n.trshkv@gmail.com>
2022-07-01 16:33:50 +0300

    feat: add Illustration component

---
+++ src/components/Illustration/Illustration.tsx
@@ -0,0 +1,47 @@
+import {useEffect, useState} from 'react';
+import cn from 'bem-cn-lite';
+import {useThemeValue} from '@yandex-cloud/uikit';
+
+export interface IllustrationProps {
+    name: string;
+    className?: string;
+}
+
+type IllustrationStore = Record<string, Record<string, () => Promise<{default: any}>>>;
+
+const store: IllustrationStore = {
+    light: {
+        403: () => import('../../assets/illustrations/light/403.svg'),
+    },
+    dark: {
+        403: () => import('../../assets/illustrations/dark/403.svg'),
+    },
+};
+
+const b = cn('kv-illustration');
+
+export const Illustration = ({name, className, ...props}: IllustrationProps) => {
+    const theme = useThemeValue();
+    const [src, setSrc] = useState('');
+    const srcGetter = store[theme] && store[theme][name];
+
+    useEffect(() => {
+        if (typeof srcGetter === 'function') {
+            srcGetter()
+                .then((svg) => setSrc(svg.default))
+                .catch((e) => {
+                    console.error(e);
+                    setSrc('');
+                });
+        }
+    }, [srcGetter]);
+
+    return (
+        <img
+            alt={name}
+            src={src}
+            className={b(null, className)}
+            {...props}
+        />
+    );
+}
