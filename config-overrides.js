89be2a169aeb8cb358426a38f403c4abec2f6ff8
ariser <n.trshkv@gmail.com>
2022-08-09 02:39:35 +0300

    test: setup test environment

--- config-overrides.js
+++ config-overrides.js
@@ -1,22 +1,29 @@
-const _ = require('lodash');
 const path = require('path');
 
 const srcRoot = path.resolve(__dirname, 'src');
 const uiKitRoot = path.resolve(__dirname, 'node_modules/@yandex-cloud/uikit');
 
-module.exports = function override(config, env) {
-    //do stuff with the webpack config...
+module.exports = {
+    webpack: (config, env) => {
+        //do stuff with the webpack config...
 
-    const oneOfRule = config.module.rules.find((r) => r.oneOf);
-    oneOfRule.oneOf.splice(0, 0, {
-        test: /\.svg$/,
-        include: [path.resolve(srcRoot, 'assets/icons'), path.resolve(uiKitRoot, 'assets/icons')],
-        loader: '@svgr/webpack',
-        options: {dimensions: false},
-    });
-    if (env === 'production') {
-        config.output.publicPath = 'resources/';
-        config.output.path = path.resolve(__dirname, 'build/');
-    }
-    return config;
+        const oneOfRule = config.module.rules.find((r) => r.oneOf);
+        oneOfRule.oneOf.splice(0, 0, {
+            test: /\.svg$/,
+            include: [path.resolve(srcRoot, 'assets/icons'), path.resolve(uiKitRoot, 'assets/icons')],
+            loader: '@svgr/webpack',
+            options: {dimensions: false},
+        });
+        if (env === 'production') {
+            config.output.publicPath = 'resources/';
+            config.output.path = path.resolve(__dirname, 'build/');
+        }
+        return config;
+    },
+    jest: (config) => {
+        // see https://github.com/timarney/react-app-rewired/issues/241
+        config.transformIgnorePatterns = ["node_modules/(?!(@yandex-cloud)/)"];
+
+        return config;
+    },
 };
