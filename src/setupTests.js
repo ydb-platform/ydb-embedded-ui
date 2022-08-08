566fd2bdc51478115ccae88405a286fcfcc08fca
ariser <n.trshkv@gmail.com>
2022-08-09 02:39:35 +0300

    test: setup test environment

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import {configure as configureUiKit} from '@yandex-cloud/uikit';
+import {configure as configureYdbUiComponents} from 'ydb-ui-components';
+import {i18n, Lang} from '../src/utils/i18n';
+
+i18n.setLang(Lang.En);
+configureYdbUiComponents({lang: Lang.En});
+configureUiKit({lang: Lang.En});
