d235253d200861c486b26a526db2c0218b864136
ariser <n.trshkv@gmail.com>
2022-08-25 02:13:08 +0300

    feat(Storage): vividly show degraded disks count

--- src/containers/Storage/utils/index.ts
+++ src/containers/Storage/utils/index.ts
@@ -1,6 +1,42 @@
 import type {TVDiskStateInfo, TVSlotId} from '../../../types/api/storage';
+import type {IStoragePoolGroup} from '../../../types/store/storage';
 
 export * from './constants';
 
 export const isFullDonorData = (donor: TVDiskStateInfo | TVSlotId): donor is TVDiskStateInfo =>
     'VDiskId' in donor;
+
+const generateEvaluator = (warn: number, crit: number) =>
+    (value: number) => {
+        if (0 <= value && value < warn) {
+            return 'success';
+        }
+
+        if (warn <= value && value < crit) {
+            return 'warning';
+        }
+
+        if (crit <= value) {
+            return 'danger';
+        }
+
+        return undefined;
+    };
+
+const defaultDegradationEvaluator = generateEvaluator(1, 2);
+
+const degradationEvaluators = {
+    'block-4-2': generateEvaluator(1, 2),
+    'mirror-3-dc': generateEvaluator(1, 3),
+};
+
+const canEvaluateErasureSpecies = (value?: string): value is keyof typeof degradationEvaluators =>
+    value !== undefined && value in degradationEvaluators;
+
+export const getDegradedSeverity = (group: IStoragePoolGroup) => {
+    const evaluate = canEvaluateErasureSpecies(group.ErasureSpecies) ?
+        degradationEvaluators[group.ErasureSpecies] :
+        defaultDegradationEvaluator;
+
+    return evaluate(group.Missing);
+};
---
+++ src/types/store/storage.ts
@@ -0,0 +1,11 @@
+import type {TBSGroupStateInfo} from '../api/storage';
+
+export interface IStoragePoolGroup extends TBSGroupStateInfo {
+    Read: number;
+    Write: number;
+    PoolName?: string;
+    Used: number;
+    Limit: number;
+    Missing: number;
+    UsedSpaceFlag: number;
+}
