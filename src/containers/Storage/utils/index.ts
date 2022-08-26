aee67f9314341c995e2c9468f5eedc48fa0a3d35
ariser <n.trshkv@gmail.com>
2022-08-26 13:36:29 +0300

    fix(Storage): properly display usage for 0 storage

--- src/containers/Storage/utils/index.ts
+++ src/containers/Storage/utils/index.ts
@@ -44,7 +44,8 @@ export const getDegradedSeverity = (group: IStoragePoolGroup) => {
 export const getUsageSeverity = generateEvaluator(80, 85);
 
 export const getUsage = (data: IStoragePoolGroup, step = 1) => {
-    const usage = Math.round((data.Used * 100) / data.Limit);
+    // if limit is 0, display 0
+    const usage = Math.round((data.Used * 100) / data.Limit) || 0;
 
     return Math.floor(usage / step) * step;
 };
