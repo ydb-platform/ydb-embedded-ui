11357367a0ba66ff2a681318028864c8259ff174
ariser <n.trshkv@gmail.com>
2022-08-30 02:04:12 +0300

    refactor(Storage): redo severity evaluation for more use cases

--- src/containers/Storage/utils/index.ts
+++ src/containers/Storage/utils/index.ts
@@ -6,28 +6,32 @@ export * from './constants';
 export const isFullDonorData = (donor: TVDiskStateInfo | TVSlotId): donor is TVDiskStateInfo =>
     'VDiskId' in donor;
 
-const generateEvaluator = (warn: number, crit: number) =>
+const generateEvaluator = <
+    OkLevel extends string,
+    WarnLevel extends string,
+    CritLevel extends string
+>(warn: number, crit: number, levels: [OkLevel, WarnLevel, CritLevel]) =>
     (value: number) => {
         if (0 <= value && value < warn) {
-            return 'success';
+            return levels[0];
         }
 
         if (warn <= value && value < crit) {
-            return 'warning';
+            return levels[1];
         }
 
         if (crit <= value) {
-            return 'danger';
+            return levels[2];
         }
 
         return undefined;
     };
 
-const defaultDegradationEvaluator = generateEvaluator(1, 2);
+const defaultDegradationEvaluator = generateEvaluator(1, 2, ['success', 'warning', 'danger']);
 
 const degradationEvaluators = {
-    'block-4-2': generateEvaluator(1, 2),
-    'mirror-3-dc': generateEvaluator(1, 3),
+    'block-4-2': generateEvaluator(1, 2, ['success', 'warning', 'danger']),
+    'mirror-3-dc': generateEvaluator(1, 3, ['success', 'warning', 'danger']),
 };
 
 const canEvaluateErasureSpecies = (value?: string): value is keyof typeof degradationEvaluators =>
@@ -41,7 +45,7 @@ export const getDegradedSeverity = (group: IStoragePoolGroup) => {
     return evaluate(group.Missing);
 };
 
-export const getUsageSeverity = generateEvaluator(80, 85);
+export const getUsageSeverity = generateEvaluator(80, 85, ['success', 'warning', 'danger']);
 
 export const getUsage = (data: IStoragePoolGroup, step = 1) => {
     // if limit is 0, display 0
