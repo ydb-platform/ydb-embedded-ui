9e5f69f661a34ffc6c969f8ce8a5521178b75077
ariser <n.trshkv@gmail.com>
2022-08-31 01:32:22 +0300

    refactor(Storage): migrate StorageFilter to TS

---
+++ src/containers/Storage/StorageFilter/StorageFilter.tsx
@@ -0,0 +1,52 @@
+import {useEffect, useRef, useState} from 'react';
+
+import {TextInput} from '@yandex-cloud/uikit';
+
+interface StorageFilterProps {
+    className?: string;
+    value?: string;
+    placeholder?: string;
+    onChange?: (value: string) => void;
+    debounce?: number;
+}
+
+export const StorageFilter = (props: StorageFilterProps) => {
+    const {
+        className,
+        value = '',
+        placeholder,
+        onChange,
+        debounce = 200,
+    } = props;
+    const [filterValue, setFilterValue] = useState(value);
+    const timer = useRef<number>();
+
+    useEffect(() => {
+        setFilterValue((prevValue) => {
+            if (prevValue !== value) {
+                return value;
+            }
+
+            return prevValue;
+        });
+    }, [value]);
+
+    const changeFilter = (newValue: string) => {
+        setFilterValue(newValue);
+
+        window.clearTimeout(timer.current);
+        timer.current = window.setTimeout(() => {
+            onChange?.(newValue);
+        }, debounce);
+    };
+
+    return (
+        <TextInput
+            className={className}
+            placeholder={placeholder}
+            value={filterValue}
+            onUpdate={changeFilter}
+            hasClear
+        />
+    );
+}
