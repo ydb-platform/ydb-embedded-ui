c895477bf1d7cdd5c0f5dfa53d54d08a985ac406
ariser <n.trshkv@gmail.com>
2022-10-04 13:12:59 +0300

    fix: fix production assets paths

--- prepare-build.sh
+++ prepare-build.sh
@@ -5,6 +5,8 @@ set -e
 cd build
 mv static resources
 mv favicon.png resources/
+mv media/* resources/media
+rm -r media
 
 if [ "$(uname)" = "Darwin" ]; then # MacOSX case
     sed -i '' -e 's/static\///g; s/favicon\.png/resources\/favicon\.png/' index.html
