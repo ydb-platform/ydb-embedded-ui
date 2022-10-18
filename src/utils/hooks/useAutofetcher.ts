4063cb1411338d351b612fc46c06bcc708fe32f1
ariser <n.trshkv@gmail.com>
2022-10-19 00:39:09 +0300

    fix(useAutofetcher): pass argument to indicate background fetch

--- src/utils/hooks/useAutofetcher.ts
+++ src/utils/hooks/useAutofetcher.ts
@@ -2,7 +2,11 @@ import {DependencyList, useEffect, useRef} from 'react';
 
 import {AutoFetcher} from '../autofetcher';
 
-export const useAutofetcher = (fetchData: VoidFunction, deps: DependencyList, enabled = true) => {
+export const useAutofetcher = (
+    fetchData: (isBackground: boolean) => void,
+    deps: DependencyList,
+    enabled = true,
+) => {
     const ref = useRef<AutoFetcher | null>(null);
 
     if (ref.current === null) {
@@ -12,14 +16,16 @@ export const useAutofetcher = (fetchData: VoidFunction, deps: DependencyList, en
     const autofetcher = ref.current;
 
     // initial fetch
-    useEffect(fetchData, deps); // eslint-disable-line react-hooks/exhaustive-deps
+    useEffect(() => {
+        fetchData(false);
+    }, deps); // eslint-disable-line react-hooks/exhaustive-deps
 
     useEffect(() => {
         autofetcher.stop();
 
         if (enabled) {
             autofetcher.start();
-            autofetcher.fetch(fetchData);
+            autofetcher.fetch(() => fetchData(true));
         }
 
         return () => {
