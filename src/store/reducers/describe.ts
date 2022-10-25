73333f57bfbaa15be35d99174441fd3840ba5899
mufazalov <mufazalov@yandex-team.ru>
2022-10-25 15:15:09 +0300

    refactor(Consumers): add selector for consumers data

--- src/store/reducers/describe.ts
+++ src/store/reducers/describe.ts
@@ -1,13 +1,16 @@
+import {createSelector} from 'reselect';
+
 import '../../services/api';
 import {TEvDescribeSchemeResult} from '../../types/api/schema';
+import {IConsumer} from '../../types/api/consumers';
 import {createRequestActionTypes, createApiRequest, ApiRequestAction} from '../utils';
 
 const FETCH_DESCRIBE = createRequestActionTypes('describe', 'FETCH_DESCRIBE');
 
-const describe = function z(
+const describe = (
     state = {loading: false, wasLoaded: false, data: {}},
     action: ApiRequestAction<typeof FETCH_DESCRIBE, TEvDescribeSchemeResult, unknown>,
-) {
+) => {
     switch (action.type) {
         case FETCH_DESCRIBE.REQUEST: {
             return {
@@ -16,10 +19,14 @@ const describe = function z(
             };
         }
         case FETCH_DESCRIBE.SUCCESS: {
-            const newData = JSON.parse(JSON.stringify(state.data));
+            let newData;
 
-            // In  case of successful fetch data should always has some Path, so we could cast type here
-            newData[action.data.Path as string] = action.data;
+            if (action.data.Path) {
+                newData = JSON.parse(JSON.stringify(state.data));
+                newData[action.data.Path] = action.data;
+            } else {
+                newData = state.data;
+            }
 
             return {
                 ...state,
@@ -41,6 +48,18 @@ const describe = function z(
             return state;
     }
 };
+
+// Consumers selectors
+const selectConsumersNames = (state: any, path: string): string[] | undefined =>
+    state.describe.data[path]?.PathDescription?.PersQueueGroup?.PQTabletConfig?.ReadRules;
+
+export const selectConsumers = createSelector(selectConsumersNames, (names = []): IConsumer[] => {
+    const consumers = names.map((name) => {
+        return {name};
+    });
+
+    return consumers;
+});
 
 export function getDescribe({path}: {path: string}) {
     return createApiRequest({
---
+++ src/types/api/consumers.ts
@@ -0,0 +1,3 @@
+export interface IConsumer {
+    name: string;
+}
