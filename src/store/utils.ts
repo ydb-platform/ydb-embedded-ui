fb946e79756f5e2402b97b9cb6b0a5278c631bb8
mufazalov <mufazalov@yandex-team.ru>
2022-11-02 15:38:24 +0300

    fix(createApiRequest): enable to process requests array and params

--- src/store/utils.ts
+++ src/store/utils.ts
@@ -21,33 +21,45 @@ export function createRequestActionTypes<Prefix extends string, Type extends str
 const isAxiosResponse = (response: any): response is AxiosResponse =>
     response && 'status' in response;
 
-type CreateApiRequestParams<Actions, Response, HandledResponse> = {
+type CreateApiRequestParams<Actions, Response, HandledResponse, AdditionalParams> = {
     actions: Actions;
-    request: Promise<Response>;
-    dataHandler?: (data: Response, getState?: () => any) => HandledResponse;
+    request: Promise<Response> | Promise<Response>[];
+    dataHandler?: (data: Response | Response[], getState?: () => any) => HandledResponse;
+    additionalParams?: AdditionalParams;
 };
 
 export function createApiRequest<
     Actions extends ReturnType<typeof createRequestActionTypes>,
     Response,
     HandledResponse,
+    AdditionalParams = unknown,
 >({
     actions,
     request,
     dataHandler = nop,
-}: CreateApiRequestParams<Actions, Response, HandledResponse>) {
+    additionalParams,
+}: CreateApiRequestParams<Actions, Response, HandledResponse, AdditionalParams>) {
     const doRequest = async function (dispatch: Dispatch, getState: () => any) {
         dispatch({
             type: actions.REQUEST,
+            additionalParams,
         });
 
         try {
-            const result = await request;
+            let result;
+
+            if (Array.isArray(request)) {
+                result = await Promise.all(request);
+            } else {
+                result = await request;
+            }
+
             const data = dataHandler(result, getState);
 
             dispatch({
                 type: actions.SUCCESS,
                 data,
+                additionalParams,
             });
 
             return data;
@@ -68,6 +80,7 @@ export function createApiRequest<
             dispatch({
                 type: actions.FAILURE,
                 error,
+                additionalParams,
             });
 
             // TODO should probably throw the received error here, but this change requires a thorough revision of all api calls
@@ -82,15 +95,19 @@ export type ApiRequestAction<
     Actions extends ReturnType<typeof createRequestActionTypes>,
     SuccessResponse = unknown,
     ErrorResponse = unknown,
+    AdditionalParams = unknown,
 > =
     | {
           type: Actions['REQUEST'];
+          additionalParams?: AdditionalParams;
       }
     | {
           type: Actions['SUCCESS'];
           data: SuccessResponse;
+          additionalParams?: AdditionalParams;
       }
     | {
           type: Actions['FAILURE'];
           error: ErrorResponse;
+          additionalParams?: AdditionalParams;
       };
