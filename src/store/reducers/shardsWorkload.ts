533351548b457e0b3f1615563ee85123ed280912
ariser <n.trshkv@gmail.com>
2022-12-07 17:56:20 +0300

    refactor(TopShards): migrate reducer to ts

--- src/store/reducers/shardsWorkload.ts
+++ src/store/reducers/shardsWorkload.ts
@@ -1,10 +1,13 @@
+import type {Reducer} from 'redux';
+
 import '../../services/api';
+import type {IShardsWorkloadAction, IShardsWorkloadState} from '../../types/store/shardsWorkload';
 
 import {parseQueryAPIExecuteResponse} from '../../utils/query';
 
 import {createRequestActionTypes, createApiRequest} from '../utils';
 
-const SEND_SHARD_QUERY = createRequestActionTypes('query', 'SEND_SHARD_QUERY');
+export const SEND_SHARD_QUERY = createRequestActionTypes('query', 'SEND_SHARD_QUERY');
 const SET_SHARD_QUERY_OPTIONS = 'query/SET_SHARD_QUERY_OPTIONS';
 
 const initialState = {
@@ -12,18 +15,21 @@ const initialState = {
     wasLoaded: false,
 };
 
-function formatSortOrder({columnId, order}) {
+export interface SortOrder {
+    columnId: string;
+    order: string;
+}
+
+function formatSortOrder({columnId, order}: SortOrder) {
     return `${columnId} ${order}`;
 }
 
-function createShardQuery(path, sortOrder, tenantName) {
-    const orderBy = Array.isArray(sortOrder) ?
-        `ORDER BY ${sortOrder.map(formatSortOrder).join(', ')}` :
-        '';
+function createShardQuery(path: string, sortOrder?: SortOrder[], tenantName?: string) {
+    const orderBy = sortOrder ? `ORDER BY ${sortOrder.map(formatSortOrder).join(', ')}` : '';
 
-    const pathSelect = tenantName ?
-        `CAST(SUBSTRING(CAST(Path AS String), ${tenantName.length}) AS Utf8) AS Path` :
-        'Path';
+    const pathSelect = tenantName
+        ? `CAST(SUBSTRING(CAST(Path AS String), ${tenantName.length}) AS Utf8) AS Path`
+        : 'Path';
 
     return `SELECT
     ${pathSelect},
@@ -40,7 +46,10 @@ LIMIT 20`;
 
 const queryAction = 'execute-scan';
 
-const shardsWorkload = (state = initialState, action) => {
+const shardsWorkload: Reducer<IShardsWorkloadState, IShardsWorkloadAction> = (
+    state = initialState,
+    action,
+) => {
     switch (action.type) {
         case SEND_SHARD_QUERY.REQUEST: {
             return {
@@ -76,7 +85,13 @@ const shardsWorkload = (state = initialState, action) => {
     }
 };
 
-export const sendShardQuery = ({database, path = '', sortOrder}) => {
+interface SendShardQueryParams {
+    database?: string;
+    path?: string;
+    sortOrder?: SortOrder[];
+}
+
+export const sendShardQuery = ({database, path = '', sortOrder}: SendShardQueryParams) => {
     return createApiRequest({
         request: window.api.sendQuery({
             schema: 'modern',
@@ -91,11 +106,11 @@ export const sendShardQuery = ({database, path = '', sortOrder}) => {
     });
 };
 
-export function setShardQueryOptions(options) {
+export function setShardQueryOptions(options: Partial<IShardsWorkloadState>) {
     return {
         type: SET_SHARD_QUERY_OPTIONS,
         data: options,
-    };
+    } as const;
 }
 
 export default shardsWorkload;
