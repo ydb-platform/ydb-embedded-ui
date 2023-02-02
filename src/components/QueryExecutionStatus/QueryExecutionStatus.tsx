4b0d84b550deb41a140d4a3d215e52084507a558
ariser <n.trshkv@gmail.com>
2023-02-03 00:43:27 +0300

    fix(QueryEditor): result status for aborted connection

--- src/components/QueryExecutionStatus/QueryExecutionStatus.tsx
+++ src/components/QueryExecutionStatus/QueryExecutionStatus.tsx
@@ -1,6 +1,12 @@
+import type {ReactNode} from 'react';
+import type {AxiosError} from 'axios';
 import cn from 'bem-cn-lite';
 
+import {Icon as UiKitIcon} from '@gravity-ui/uikit';
+
 import Icon from '../Icon/Icon';
+
+import questionIcon from '../../assets/icons/question.svg';
 
 import './QueryExecutionStatus.scss';
 
@@ -8,12 +14,19 @@ const b = cn('kv-query-execution-status');
 
 interface QueryExecutionStatusProps {
     className?: string;
-    hasError?: boolean;
+    error?: AxiosError | Record<string, any>;
 }
 
-function QueryExecutionStatus({className, hasError}: QueryExecutionStatusProps) {
-    return hasError === undefined ? null : (
-        <div className={b(null, className)}>
+export const QueryExecutionStatus = ({className, error}: QueryExecutionStatusProps) => {
+    let icon: ReactNode;
+    let label: string;
+
+    if (error?.code === 'ECONNABORTED') {
+        icon = <UiKitIcon data={questionIcon} size={16} />;
+        label = 'Connection aborted';
+    } else {
+        const hasError = Boolean(error);
+        icon = (
             <Icon
                 name={hasError ? 'failure' : 'success'}
                 viewBox={hasError ? '0 0 512 512' : '0 0 16 16'}
@@ -21,9 +34,14 @@ function QueryExecutionStatus({className, hasError}: QueryExecutionStatusProps) 
                 height={16}
                 className={b('result-status-icon', {error: hasError})}
             />
-            {hasError ? 'Failed' : 'Completed'}
+        );
+        label = hasError ? 'Failed' : 'Completed';
+    }
+
+    return (
+        <div className={b(null, className)}>
+            {icon}
+            {label}
         </div>
     );
-}
-
-export default QueryExecutionStatus;
+};
