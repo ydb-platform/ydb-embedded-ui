2e2ce5ec0bb25fdf9becabb7cfa7777992e07053
mufazalov <mufazalov@yandex-team.ru>
2023-03-16 14:26:39 +0300

    test: setup playwright e2e tests

---
+++ playwright.config.ts
@@ -0,0 +1,26 @@
+import {PlaywrightTestConfig, devices} from '@playwright/test';
+
+const baseUrl = process.env.PLAYWRIGHT_BASE_URL;
+
+const config: PlaywrightTestConfig = {
+    testDir: 'tests',
+    timeout: 2 * 60 * 1000,
+    // If there is no url provided, playwright starts webServer with the app in dev mode
+    webServer: baseUrl
+        ? undefined
+        : {
+              command: 'npm run dev',
+              port: 3000,
+          },
+    use: {
+        baseURL: baseUrl || 'http://localhost:3000',
+    },
+    projects: [
+        {
+            name: 'chromium',
+            use: {...devices['Desktop Chrome']},
+        },
+    ],
+};
+
+export default config;
