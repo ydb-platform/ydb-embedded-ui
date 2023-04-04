4ef459adca6cb9135c2f0cbce3e84282cd73ed5a
mufazalov <mufazalov@yandex-team.ru>
2023-04-04 13:09:47 +0300

    test: add suit for Storage page

---
+++ tests/suites/storage/storage.test.ts
@@ -0,0 +1,31 @@
+import {test, expect} from '@playwright/test';
+
+import {StoragePage} from './StoragePage';
+
+test.describe('Test Storage page', async () => {
+    test('Storage page is OK', async ({page}) => {
+        const storagePage = new StoragePage(page);
+        const response = await storagePage.goto();
+        expect(response?.ok()).toBe(true);
+    });
+
+    test('Storage page has groups table', async ({page}) => {
+        const storagePage = new StoragePage(page);
+        await storagePage.goto();
+
+        await storagePage.selectVisibleEntityType('All');
+        await storagePage.selectEntityType('Groups');
+
+        await expect(storagePage.table).toBeVisible();
+    });
+
+    test('Storage page has nodes table', async ({page}) => {
+        const storagePage = new StoragePage(page);
+        await storagePage.goto();
+
+        await storagePage.selectVisibleEntityType('All');
+        await storagePage.selectEntityType('Nodes');
+
+        await expect(storagePage.table).toBeVisible();
+    });
+});
