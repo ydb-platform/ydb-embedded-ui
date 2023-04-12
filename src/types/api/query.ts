a7b45dcfffbcd51fdd8c585fa3952ce2a487138b
mufazalov <mufazalov@yandex-team.ru>
2023-04-12 18:28:10 +0300

    chore: add query types

--- src/types/api/query.ts
+++ src/types/api/query.ts
@@ -1,8 +1,163 @@
+// ==== types from backend protos ====
+interface Position {
+    row?: number;
+    column?: number;
+    file?: string;
+}
+
+interface IssueMessage {
+    position?: Position;
+    end_position?: Position;
+    message?: string;
+    issue_code?: number;
+    severity?: number;
+    issues?: IssueMessage[];
+}
+
+/** incomplete */
+interface TDqStageStats {}
+
+/** incomplete */
+interface TKqpStatsCompile {}
+
+interface TDqTableStats {
+    TablePath?: string;
+    /** uint64 */
+    ReadRows?: string;
+    /** uint64 */
+    ReadBytes?: string;
+    /** uint64 */
+    WriteRows?: string;
+    /** uint64 */
+    WriteBytes?: string;
+    /** uint64 */
+    EraseRows?: string;
+    /** uint64 */
+    EraseBytes?: string;
+    AffectedPartitions?: number;
+    Extra?: unknown;
+}
+
+interface TDqExecutionStats {
+    /** uint64 */
+    CpuTimeUs?: string;
+    /** uint64 */
+    DurationUs?: string;
+    /** uint64 */
+    ResultRows?: string;
+    /** uint64 */
+    ResultBytes?: string;
+
+    Tables?: TDqTableStats[];
+
+    /** uint64 */
+    ExecuterCpuTimeUs?: string;
+    /** uint64 */
+    StartTimeMs?: string;
+    /** uint64 */
+    FinishTimeMs?: string;
+    /** uint64 */
+    FirstRowTimeMs?: string;
+
+    Stages?: TDqStageStats[];
+    TxPlansWithStats?: string[];
+
+    Extra: unknown;
+}
+
+interface TKqpStatsQuery {
+    /** uint64 */
+    DurationUs?: string;
+    Compilation?: TKqpStatsCompile;
+
+    /** uint64 */
+    WorkerCpuTimeUs?: string;
+    /** uint64 */
+    ReadSetsCount?: string;
+    /** uint64 */
+    MaxShardProgramSize?: string;
+    /** uint64 */
+    MaxShardReplySize?: string;
+
+    Executions?: TDqExecutionStats[];
+}
+
+// ==== Self-written types ====
+// ==== Schema v0.2 ====
+
+// ==== Plan ====
+export interface PlanMeta {
+    version: string;
+    type: 'script' | 'query';
+}
+
+interface PlanTable {
+    name: string;
+    reads?: {
+        type: string;
+        columns?: string[];
+        limit?: string;
+        scan_by?: string[];
+    }[];
+    writes?: {
+        type: string;
+        columns?: string[];
+    }[];
+}
+
+interface PlanNodeStats {
+    TotalCpuTimeUs?: number;
+    TotalTasks?: number;
+    TotalInputBytes?: number;
+    TotalInputRows?: number;
+    TotalOutputBytes?: number;
+    TotalDurationMs?: number;
+    TotalOutputRows?: number;
+}
+
+interface PlanNodeOperator {
+    Name: string;
+    Limit?: string;
+    ReadLimit?: string;
+    ReadColumns?: string[];
+    ReadRanges?: string[];
+    Table?: string;
+    Iterator?: string;
+}
+
+export interface PlanNode {
+    PlanNodeId?: number;
+    'Node Type'?: string;
+    Plans?: PlanNode[];
+    Operators?: PlanNodeOperator[];
+    Tables?: string[];
+    PlanNodeType?: string;
+    Stats?: PlanNodeStats;
+    'CTE Name'?: string;
+    'Subplan Name'?: string;
+    'Parent Relationship'?: string;
+}
+
+interface ScriptPlan {
+    queries: {
+        Plan: PlanNode;
+        tables?: PlanTable[];
+    }[];
+    meta: PlanMeta;
+}
+
+interface ScanPlan {
+    Plan: PlanNode;
+    tables: PlanTable[];
+    meta: PlanMeta;
+}
+
+// ====================
 // common
 
-type Plan = Record<string, any>;
+type Plan = ScanPlan | ScriptPlan;
 type AST = string;
-type Stats = Record<string, any>;
+type Stats = TKqpStatsQuery;
 
 export interface CommonFields {
     ast?: AST;
@@ -15,8 +170,8 @@ interface DeprecatedCommonFields {
 }
 
 export interface ErrorResponse {
-    error?: any;
-    issues?: any;
+    error?: IssueMessage;
+    issues?: IssueMessage[];
 }
 
 export type ExecuteActions = 'execute-script' | 'execute' | 'execute-scan' | undefined;
@@ -109,7 +264,7 @@ type DeprecatedExplainResponse<Action extends ExplainActions> =
         ? ({result: {ast: AST}} & Required<DeprecatedCommonFields>) | {ast: AST}
         : Action extends 'explain'
             ? ({result: Plan} & Required<DeprecatedCommonFields>) | Plan
-            : unknown;
+    : unknown;
 
 // ==== COMBINED API RESPONSE ====
 
