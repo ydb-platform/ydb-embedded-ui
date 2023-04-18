fe37c67e5e1cdd8176fbabc8b9297427d1ca22b2
mufazalov <mufazalov@yandex-team.ru>
2023-04-18 17:20:36 +0300

    fix(QueryExplain): request AST if it is empty

--- src/containers/Tenant/QueryEditor/QueryExplain/QueryExplain.js
+++ src/containers/Tenant/QueryEditor/QueryExplain/QueryExplain.js
@@ -108,7 +108,7 @@ function QueryExplain(props) {
         if (!props.ast && activeOption === ExplainOptionIds.ast) {
             props.astQuery();
         }
-    }, [activeOption]);
+    }, [activeOption, props.ast]);
 
     const onSelectOption = (tabId) => {
         setActiveOption(tabId);
