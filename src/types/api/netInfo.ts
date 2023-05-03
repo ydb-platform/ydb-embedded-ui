a28a943cda4734daaae3cf1836296c934407bf88
mufazalov <mufazalov@yandex-team.ru>
2023-05-03 15:59:04 +0300

    chore: cover netinfo request with types

---
+++ src/types/api/netInfo.ts
@@ -0,0 +1,48 @@
+import type {EFlag} from './enums';
+
+/**
+ * endpoint: /viewer/json/netinfo
+ *
+ * source: https://github.com/ydb-platform/ydb/blob/main/ydb/core/viewer/protos/viewer.proto
+ */
+export interface TNetInfo {
+    Overall: EFlag;
+    Tenants?: TNetTenantInfo[];
+}
+
+interface TNetTenantInfo {
+    Overall: EFlag;
+    Name: string;
+    Nodes?: TNetNodeInfo[];
+}
+
+interface TNetNodeInfo {
+    NodeId: number;
+    Overall: EFlag;
+    Peers?: TNetNodePeerInfo[];
+    NodeType: ENodeType;
+    DataCenter: string;
+    Rack: string;
+    Host: string;
+    Port: number;
+}
+
+interface TNetNodePeerInfo {
+    NodeId: number;
+    PeerName: string;
+    Connected: boolean;
+    ConnectStatus: EFlag;
+    /** uint64 */
+    ChangeTime: string;
+    NodeType: ENodeType;
+    DataCenter: string;
+    Rack: string;
+    Host: string;
+    Port: number;
+}
+
+enum ENodeType {
+    UnknownNodeType,
+    Static,
+    Dynamic,
+}
