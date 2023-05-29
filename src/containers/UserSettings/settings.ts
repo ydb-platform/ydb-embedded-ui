1929b68d8569b4eae81eaa742db14f683a7369f3
Артём Муфазалов <67755036+artemmufazalov@users.noreply.github.com>
2023-05-29 14:33:23 +0300

    refactor(UserSettings): separate settings (#404)

---
+++ src/containers/UserSettings/settings.ts
@@ -0,0 +1,91 @@
+import type {IconProps} from '@gravity-ui/uikit';
+
+import favoriteFilledIcon from '../../assets/icons/star.svg';
+import flaskIcon from '../../assets/icons/flask.svg';
+
+import {
+    ENABLE_QUERY_MODES_FOR_EXPLAIN,
+    INVERTED_DISKS_KEY,
+    THEME_KEY,
+    USE_NODES_ENDPOINT_IN_DIAGNOSTICS_KEY,
+} from '../../utils/constants';
+
+import type {SettingProps} from './Setting';
+import i18n from './i18n';
+
+export interface SettingsSection {
+    id: string;
+    title: string;
+    settings: SettingProps[];
+}
+
+export interface SettingsPage {
+    id: string;
+    title: string;
+    icon: IconProps;
+    sections: SettingsSection[];
+}
+
+export type YDBEmbeddedUISettings = SettingsPage[];
+
+const themeOptions = [
+    {
+        value: 'system',
+        content: i18n('settings.theme.option-system'),
+    },
+    {
+        value: 'light',
+        content: i18n('settings.theme.option-light'),
+    },
+    {
+        value: 'dark',
+        content: i18n('settings.theme.option-dark'),
+    },
+];
+
+export const themeSetting: SettingProps = {
+    settingKey: THEME_KEY,
+    title: i18n('settings.theme.title'),
+    type: 'radio',
+    options: themeOptions,
+};
+export const invertedDisksSetting: SettingProps = {
+    settingKey: INVERTED_DISKS_KEY,
+    title: i18n('settings.invertedDisks.title'),
+};
+export const useNodesEndpointSetting: SettingProps = {
+    settingKey: USE_NODES_ENDPOINT_IN_DIAGNOSTICS_KEY,
+    title: i18n('settings.useNodesEndpoint.title'),
+    helpPopoverContent: i18n('settings.useNodesEndpoint.popover'),
+};
+export const enableQueryModesForExplainSetting: SettingProps = {
+    settingKey: ENABLE_QUERY_MODES_FOR_EXPLAIN,
+    title: i18n('settings.enableQueryModesForExplain.title'),
+    helpPopoverContent: i18n('settings.enableQueryModesForExplain.popover'),
+};
+
+export const generalSection: SettingsSection = {
+    id: 'generalSection',
+    title: i18n('section.general'),
+    settings: [themeSetting],
+};
+export const experimentsSection: SettingsSection = {
+    id: 'experimentsSection',
+    title: i18n('section.experiments'),
+    settings: [invertedDisksSetting, useNodesEndpointSetting, enableQueryModesForExplainSetting],
+};
+
+export const generalPage: SettingsPage = {
+    id: 'generalPage',
+    title: i18n('page.general'),
+    icon: {data: favoriteFilledIcon, height: 14, width: 14},
+    sections: [generalSection],
+};
+export const experimentsPage: SettingsPage = {
+    id: 'experimentsPage',
+    title: i18n('page.experiments'),
+    icon: {data: flaskIcon},
+    sections: [experimentsSection],
+};
+
+export const settings: YDBEmbeddedUISettings = [generalPage, experimentsPage];
