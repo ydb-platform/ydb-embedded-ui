e9e649f614691e44172c9b93dd3119066c145413
Артём Муфазалов <67755036+artemmufazalov@users.noreply.github.com>
2023-06-21 11:18:32 +0300

    fix(Cluster): add icons to tabs (#430)

--- src/containers/Cluster/utils.tsx
+++ src/containers/Cluster/utils.tsx
@@ -1,3 +1,10 @@
+import {Icon} from '@gravity-ui/uikit';
+import cubes3Icon from '@gravity-ui/icons/svgs/cubes-3.svg';
+import databasesIcon from '@gravity-ui/icons/svgs/databases.svg';
+import hardDriveIcon from '@gravity-ui/icons/svgs/hard-drive.svg';
+
+import versionsIcon from '../../assets/icons/versions.svg';
+
 import type {ValueOf} from '../../types/common';
 import routes, {createHref} from '../../routes';
 
@@ -13,18 +20,22 @@ export type ClusterTab = ValueOf<typeof clusterTabsIds>;
 const tenants = {
     id: clusterTabsIds.tenants,
     title: 'Databases',
+    icon: <Icon data={databasesIcon} />,
 };
 const nodes = {
     id: clusterTabsIds.nodes,
     title: 'Nodes',
+    icon: <Icon data={cubes3Icon} />,
 };
 const storage = {
     id: clusterTabsIds.storage,
     title: 'Storage',
+    icon: <Icon data={hardDriveIcon} />,
 };
 const versions = {
     id: clusterTabsIds.versions,
     title: 'Versions',
+    icon: <Icon data={versionsIcon} />,
 };
 
 export const clusterTabs = [tenants, nodes, storage, versions];
