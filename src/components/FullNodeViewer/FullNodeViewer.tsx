1f2fc08b0c528f4368a77550302ca3836bab8a74
Артём Муфазалов <67755036+artemmufazalov@users.noreply.github.com>
2023-07-03 17:37:21 +0300

    refactor(FullNodeViewer): migrate to ts (#455)

---
+++ src/components/FullNodeViewer/FullNodeViewer.tsx
@@ -0,0 +1,73 @@
+import cn from 'bem-cn-lite';
+
+import type {TSystemStateInfo} from '../../types/api/nodes';
+
+import {LOAD_AVERAGE_TIME_INTERVALS} from '../../utils/constants';
+import {calcUptime} from '../../utils';
+
+import InfoViewer from '../InfoViewer/InfoViewer';
+import ProgressViewer from '../ProgressViewer/ProgressViewer';
+import {PoolUsage} from '../PoolUsage/PoolUsage';
+
+import './FullNodeViewer.scss';
+
+const b = cn('full-node-viewer');
+
+interface FullNodeViewerProps {
+    node: TSystemStateInfo | undefined;
+    className?: string;
+}
+
+export const FullNodeViewer = ({node, className}: FullNodeViewerProps) => {
+    const endpointsInfo = node?.Endpoints?.map(({Name, Address}) => ({
+        label: Name,
+        value: Address,
+    }));
+
+    const commonInfo = [
+        {label: 'Version', value: node?.Version},
+        {label: 'Uptime', value: calcUptime(node?.StartTime)},
+        {label: 'DC', value: node?.DataCenterDescription},
+        {label: 'Rack', value: node?.Rack},
+    ];
+
+    const averageInfo = node?.LoadAverage?.map((load, loadIndex) => ({
+        label: LOAD_AVERAGE_TIME_INTERVALS[loadIndex],
+        value: <ProgressViewer value={load} percents={true} colorizeProgress={true} />,
+    }));
+
+    return (
+        <div className={`${b()} ${className}`}>
+            {node ? (
+                <div className={b('common-info')}>
+                    <div>
+                        <div className={b('section-title')}>Pools</div>
+                        <div className={b('section', {pools: true})}>
+                            {node?.PoolStats?.map((pool, poolIndex) => (
+                                <PoolUsage key={poolIndex} data={pool} />
+                            ))}
+                        </div>
+                    </div>
+
+                    {endpointsInfo && endpointsInfo.length && (
+                        <InfoViewer
+                            title="Endpoints"
+                            className={b('section')}
+                            info={endpointsInfo}
+                        />
+                    )}
+
+                    <InfoViewer title="Common info" className={b('section')} info={commonInfo} />
+
+                    <InfoViewer
+                        title="Load average"
+                        className={b('section', {average: true})}
+                        info={averageInfo}
+                    />
+                </div>
+            ) : (
+                <div className="error">no data</div>
+            )}
+        </div>
+    );
+};
