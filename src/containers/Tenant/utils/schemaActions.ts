b02378dc050f164533ada44580420a3872f55fea
Aleksei Egorov <egorovalexei@egorovalexei-dev.vla.yp-c.yandex.net>
2023-08-10 16:14:51 +0300

    fix: fix topic templates

--- src/containers/Tenant/utils/schemaActions.ts
+++ src/containers/Tenant/utils/schemaActions.ts
@@ -59,15 +59,17 @@ const createTopicTemplate = (path: string) => {
     return `-- docs: https://ydb.tech/en/docs/yql/reference/syntax/create_topic
 CREATE TOPIC \`${path}/my_topic\` (
     CONSUMER consumer1,
-    CONSUMER consumer2 WITH (read_from = 0) -- read_from: Sets up the message write time starting from which the consumer will receive data.
+    CONSUMER consumer2 WITH (read_from = Datetime('2022-12-01T12:13:22Z')) -- Sets up the message write time starting from which the consumer will receive data.
+                                                                           -- Value type: Datetime OR Timestamp OR integer (unix-timestamp in the numeric format). 
+                                                                           -- Default value: now
 ) WITH (
-    min_active_partitions = 5, -- min_active_partitions: Minimum number of topic partitions.
-    partition_count_limit = 10, -- partition_count_limit: Maximum number of active partitions in the topic. 0 is interpreted as unlimited.
-    retention_period = Interval('PT12H'), -- retention_period: Data retention period in the topic.
-    retention_storage_mb = 1, --  retention_storage_mb: Limit on the maximum disk space occupied by the topic data. When this value is exceeded, 
-                              --  the older data is cleared, like under a retention policy. 0 is interpreted as unlimited.
-    partition_write_speed_bytes_per_second = 2097152, -- partition_write_speed_bytes_per_second: Maximum allowed write speed per partition.
-    partition_write_burst_bytes = 2097152 -- partition_write_burst_bytes: Write quota allocated for write bursts. When set to zero, the actual write_burst value is equalled 
+    min_active_partitions = 5, -- Minimum number of topic partitions.
+    partition_count_limit = 10, -- Maximum number of active partitions in the topic. 0 is interpreted as unlimited.
+    retention_period = Interval('PT12H'), -- Data retention period in the topic. Value type: Interval, default value: 18h.
+    retention_storage_mb = 1, -- Limit on the maximum disk space occupied by the topic data. When this value is exceeded, 
+                              -- the older data is cleared, like under a retention policy. 0 is interpreted as unlimited.
+    partition_write_speed_bytes_per_second = 2097152, -- Maximum allowed write speed per partition.
+    partition_write_burst_bytes = 2097152 -- Write quota allocated for write bursts. When set to zero, the actual write_burst value is equalled 
                                           -- to the quota value (this allows write bursts of up to one second).
 );`;
 };
@@ -75,23 +77,25 @@ CREATE TOPIC \`${path}/my_topic\` (
 const alterTopicTemplate = (path: string) => {
     return `-- docs: https://ydb.tech/en/docs/yql/reference/syntax/alter_topic
 ALTER TOPIC \`${path}\`
-    ADD CONSUMER new_consumer WITH (read_from = 0), -- read_from: Sets up the message write time starting from which the consumer will receive data.
-    ALTER CONSUMER consumer1 SET (read_from = 1691600896),
+    ADD CONSUMER new_consumer WITH (read_from = 0), -- Sets up the message write time starting from which the consumer will receive data.
+                                                    -- Value type: Datetime OR Timestamp OR integer (unix-timestamp in the numeric format).
+                                                    -- Default value: now
+    ALTER CONSUMER consumer1 SET (read_from = Datetime('2023-12-01T12:13:22Z')),
     DROP CONSUMER consumer2,
     SET (
-        min_active_partitions = 10, -- min_active_partitions: Minimum number of topic partitions.
-        partition_count_limit = 15, -- partition_count_limit: Maximum number of active partitions in the topic. 0 is interpreted as unlimited.
-        retention_period = Interval('PT36H'), -- retention_period: Data retention period in the topic.
-        retention_storage_mb = 10, --  retention_storage_mb: Limit on the maximum disk space occupied by the topic data. When this value is exceeded, 
-                                   --  the older data is cleared, like under a retention policy. 0 is interpreted as unlimited.
-        partition_write_speed_bytes_per_second = 3145728, -- partition_write_speed_bytes_per_second: Maximum allowed write speed per partition.
-        partition_write_burst_bytes = 1048576 -- partition_write_burst_bytes: Write quota allocated for write bursts. When set to zero, the actual write_burst value is equalled 
+        min_active_partitions = 10, -- Minimum number of topic partitions.
+        partition_count_limit = 15, -- Maximum number of active partitions in the topic. 0 is interpreted as unlimited.
+        retention_period = Interval('PT36H'), -- Data retention period in the topic. Value type: Interval, default value: 18h.
+        retention_storage_mb = 10, -- Limit on the maximum disk space occupied by the topic data. When this value is exceeded, 
+                                   -- the older data is cleared, like under a retention policy. 0 is interpreted as unlimited.
+        partition_write_speed_bytes_per_second = 3145728, -- Maximum allowed write speed per partition.
+        partition_write_burst_bytes = 1048576 -- Write quota allocated for write bursts. When set to zero, the actual write_burst value is equalled 
                                               -- to the quota value (this allows write bursts of up to one second).
     );`;
 };
 
 const dropTopicTemplate = (path: string) => {
-    return `DROP TOPIC \`${path}\``;
+    return `DROP TOPIC \`${path}\`;`;
 };
 
 interface ActionsAdditionalEffects {
