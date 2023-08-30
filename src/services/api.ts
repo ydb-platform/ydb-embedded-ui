0e54edcedfc68f11b05be01c6599b9f26e1fd03a
KROSY <krosy1337@gmail.com>
2023-08-30 15:58:26 +0300

    fix: fix Healthcheck blinking

--- src/services/api.ts
+++ src/services/api.ts
@@ -350,10 +350,14 @@ export class YdbEmbeddedAPI extends AxiosWrapper {
             enable_sampling: enableSampling,
         });
     }
-    getHealthcheckInfo(database: string) {
-        return this.get<HealthCheckAPIResponse>(this.getPath('/viewer/json/healthcheck'), {
-            tenant: database,
-        });
+    getHealthcheckInfo(database: string, {concurrentId}: AxiosOptions = {}) {
+        return this.get<HealthCheckAPIResponse>(
+            this.getPath('/viewer/json/healthcheck'),
+            {
+                tenant: database,
+            },
+            {concurrentId: concurrentId || `getHealthcheckInfo|${database}`},
+        );
     }
     killTablet(id?: string) {
         return this.get<string>(this.getPath(`/tablets?KillTabletID=${id}`), {});
