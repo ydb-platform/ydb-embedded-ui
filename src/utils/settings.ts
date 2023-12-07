894bb259eec1ec7a3db3bf771ec546e0ee8049cc
mufazalov <mufazalov@yandex-team.ru>
2023-12-07 11:58:40 +0300

    feat: always use localStorage if no settingsApi

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -3,8 +3,11 @@ import {getValueFromLS} from './utils';
 export const userSettings = window.userSettings || {};
 export const systemSettings = window.systemSettings || {};
 
+export const settingsApi = window.web_version ? systemSettings.settingsApi : undefined;
+
 export function readSavedSettingsValue(key: string, defaultValue?: string) {
-    const savedValue = window.web_version ? userSettings[key] : getValueFromLS(key);
+    // If there is no settingsApi, use localStorage
+    const savedValue = settingsApi ? userSettings[key] : getValueFromLS(key);
 
     return savedValue ?? defaultValue;
 }
