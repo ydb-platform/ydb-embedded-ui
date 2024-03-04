b597a838322b953591ec32cc2e9141c0ca601ac4
Valerii Sidorenko <balepas@nebius.com>
2024-03-04 15:52:06 +0100

    feat: allow replace ErrorBoundary compponent

--- src/components/ComponentsProvider/componentsRegistry.ts
+++ src/components/ComponentsProvider/componentsRegistry.ts
@@ -2,10 +2,12 @@ import {StaffCard} from '../User/StaffCard';
 import {AsideNavigation} from '../../containers/AsideNavigation/AsideNavigation';
 
 import {ComponentsRegistryTemplate, Registry} from './registry';
+import {ErrorBoundaryInner} from '../ErrorBoundary/ErrorBoundary';
 
 const componentsRegistryInner = new Registry()
     .register('StaffCard', StaffCard)
-    .register('AsideNavigation', AsideNavigation);
+    .register('AsideNavigation', AsideNavigation)
+    .register('ErrorBoundary', ErrorBoundaryInner);
 
 export type ComponentsRegistry = ComponentsRegistryTemplate<typeof componentsRegistryInner>;
 
--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -6,10 +6,16 @@ import {Button, Disclosure} from '@gravity-ui/uikit';
 
 import {registerError} from '../../utils/registerError';
 import {Illustration} from '../Illustration';
+import {useComponent} from '../ComponentsProvider/ComponentsProvider';
 import i18n from './i18n';
 import './ErrorBoundary.scss';
 
 const b = cn('ydb-error-boundary');
+
+export function ErrorBoundary({children}: {children?: ReactNode}) {
+    const ErrorBoundaryComponent = useComponent('ErrorBoundary');
+    return <ErrorBoundaryComponent>{children}</ErrorBoundaryComponent>;
+}
 
 interface ErrorBoundaryProps {
     children?: ReactNode;
@@ -17,7 +23,11 @@ interface ErrorBoundaryProps {
     onReportProblem?: (error?: Error) => void;
 }
 
-export const ErrorBoundary = ({children, useRetry = true, onReportProblem}: ErrorBoundaryProps) => {
+export function ErrorBoundaryInner({
+    children,
+    useRetry = true,
+    onReportProblem,
+}: ErrorBoundaryProps) {
     return (
         <ErrorBoundaryBase
             onError={(error, info) => {
@@ -25,38 +35,54 @@ export const ErrorBoundary = ({children, useRetry = true, onReportProblem}: Erro
             }}
             fallbackRender={({error, resetErrorBoundary}) => {
                 return (
-                    <div className={b(null)}>
-                        <Illustration name="error" className={b('illustration')} />
-                        <div className={b('content')}>
-                            <h2 className={b('error-title')}>{i18n('error-title')}</h2>
-                            <div className={b('error-description')}>
-                                {i18n('error-description')}
-                            </div>
-                            <Disclosure
-                                summary={i18n('show-details')}
-                                className={b('show-details')}
-                                size="m"
-                            >
-                                <pre className={b('error-details')}>{error.stack}</pre>
-                            </Disclosure>
-                            <div className={b('actions')}>
-                                {useRetry && (
-                                    <Button view="outlined" onClick={resetErrorBoundary}>
-                                        {i18n('button-reset')}
-                                    </Button>
-                                )}
-                                {onReportProblem && (
-                                    <Button view="outlined" onClick={() => onReportProblem(error)}>
-                                        {i18n('report-problem')}
-                                    </Button>
-                                )}
-                            </div>
-                        </div>
-                    </div>
+                    <ErrorBoundaryFallback
+                        error={error}
+                        useRetry={useRetry}
+                        resetErrorBoundary={resetErrorBoundary}
+                        onReportProblem={onReportProblem}
+                    />
                 );
             }}
         >
             {children}
         </ErrorBoundaryBase>
     );
-};
+}
+
+interface ErrorBoundaryFallbackProps {
+    error: Error;
+    useRetry?: boolean;
+    resetErrorBoundary: () => void;
+    onReportProblem?: (error?: Error) => void;
+}
+export function ErrorBoundaryFallback({
+    error,
+    resetErrorBoundary,
+    useRetry,
+    onReportProblem,
+}: ErrorBoundaryFallbackProps) {
+    return (
+        <div className={b()}>
+            <Illustration name="error" className={b('illustration')} />
+            <div className={b('content')}>
+                <h2 className={b('error-title')}>{i18n('error-title')}</h2>
+                <div className={b('error-description')}>{i18n('error-description')}</div>
+                <Disclosure summary={i18n('show-details')} className={b('show-details')} size="m">
+                    <pre className={b('error-details')}>{error.stack}</pre>
+                </Disclosure>
+                <div className={b('actions')}>
+                    {useRetry && (
+                        <Button view="outlined" onClick={resetErrorBoundary}>
+                            {i18n('button-reset')}
+                        </Button>
+                    )}
+                    {onReportProblem && (
+                        <Button view="outlined" onClick={() => onReportProblem(error)}>
+                            {i18n('report-problem')}
+                        </Button>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
