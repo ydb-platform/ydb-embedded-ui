b6bcd686df4e092019da0d39e9da384930cf4a0a
Артём Муфазалов <67755036+artemmufazalov@users.noreply.github.com>
2024-03-15 13:09:49 +0300

    fix(Tablet): correctly process error in dialog action (#758)

---
+++ src/components/ButtonWithConfirmDialog/ButtonWithConfirmDialog.tsx
@@ -0,0 +1,75 @@
+import {useState, type ReactNode} from 'react';
+
+import {Button, type ButtonProps} from '@gravity-ui/uikit';
+
+import {CriticalActionDialog} from '../CriticalActionDialog';
+
+interface ButtonWithConfirmDialogProps<T, K> {
+    children: ReactNode;
+    onConfirmAction: () => Promise<T>;
+    onConfirmActionSuccess?: (() => Promise<K>) | VoidFunction;
+    dialogContent: string;
+    buttonDisabled?: ButtonProps['disabled'];
+    buttonView?: ButtonProps['view'];
+    buttonClassName?: ButtonProps['className'];
+}
+
+export function ButtonWithConfirmDialog<T, K>({
+    children,
+    onConfirmAction,
+    onConfirmActionSuccess,
+    dialogContent,
+    buttonDisabled = false,
+    buttonView = 'action',
+    buttonClassName,
+}: ButtonWithConfirmDialogProps<T, K>) {
+    const [isConfirmDialogVisible, setIsConfirmDialogVisible] = useState(false);
+    const [buttonLoading, setButtonLoading] = useState(false);
+
+    const handleConfirmAction = async () => {
+        setButtonLoading(true);
+        await onConfirmAction();
+        setButtonLoading(false);
+    };
+
+    const handleConfirmActionSuccess = async () => {
+        if (onConfirmActionSuccess) {
+            setButtonLoading(true);
+
+            try {
+                await onConfirmActionSuccess();
+            } catch {
+            } finally {
+                setButtonLoading(false);
+            }
+        }
+    };
+
+    const handleConfirmActionError = () => {
+        setButtonLoading(false);
+    };
+
+    return (
+        <>
+            <CriticalActionDialog
+                visible={isConfirmDialogVisible}
+                text={dialogContent}
+                onConfirm={handleConfirmAction}
+                onConfirmActionSuccess={handleConfirmActionSuccess}
+                onConfirmActionError={handleConfirmActionError}
+                onClose={() => {
+                    setIsConfirmDialogVisible(false);
+                }}
+            />
+            <Button
+                onClick={() => setIsConfirmDialogVisible(true)}
+                view={buttonView}
+                disabled={buttonDisabled}
+                loading={!buttonDisabled && buttonLoading}
+                className={buttonClassName}
+            >
+                {children}
+            </Button>
+        </>
+    );
+}
