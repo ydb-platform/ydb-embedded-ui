5d21b05beac6d331e598acae7d92b8f92b97d00c
mufazalov <mufazalov@yandex-team.ru>
2024-05-23 13:17:46 +0300

    chore: update readme

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function (app) {
     const metaYdbBackend = process.env.META_YDB_BACKEND;
-    if (metaYdbBackend) {
+    if (metaYdbBackend && metaYdbBackend !== 'undefined') {
         app.use(
             '/meta',
             createProxyMiddleware({
