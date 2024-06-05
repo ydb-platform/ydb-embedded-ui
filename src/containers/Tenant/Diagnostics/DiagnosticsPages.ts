adaf5ef1259b9f31ce4bffd69190f45cb3f7e0cc
Vladimir Lewandowski <me@vovaspace.com>
2024-06-05 12:04:41 +0200

    fix: `ASYNC_REPLICATION_PAGES` mapping

--- src/containers/Tenant/Diagnostics/DiagnosticsPages.ts
+++ src/containers/Tenant/Diagnostics/DiagnosticsPages.ts
@@ -70,6 +70,8 @@ const partitions = {
     title: 'Partitions',
 };
 
+export const ASYNC_REPLICATION_PAGES = [overview, describe];
+
 export const DATABASE_PAGES = [
     overview,
     topQueries,
@@ -118,7 +120,7 @@ const pathTypeToPages: Record<EPathType, Page[] | undefined> = {
 
     [EPathType.EPathTypeView]: VIEW_PAGES,
 
-    [EPathType.EPathTypeReplication]: VIEW_PAGES,
+    [EPathType.EPathTypeReplication]: ASYNC_REPLICATION_PAGES,
 };
 
 export const getPagesByType = (type?: EPathType) => (type && pathTypeToPages[type]) || DIR_PAGES;
