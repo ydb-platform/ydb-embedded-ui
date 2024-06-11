3c75c7f5c69c841c71331e3f693490d573da6506
mufazalov <mufazalov@yandex-team.ru>
2024-06-11 14:36:53 +0300

    feat(SchemaViewer): refactor, add view schema

---
+++ src/containers/Tenant/Schema/SchemaViewer/columns.tsx
@@ -0,0 +1,123 @@
+import DataTable from '@gravity-ui/react-data-table';
+import {Icon} from '@gravity-ui/uikit';
+
+import i18n from './i18n';
+import {b} from './shared';
+import type {SchemaColumn, SchemaData} from './types';
+
+import keyIcon from '../../../../assets/icons/key.svg';
+
+export const SCHEMA_COLUMNS_WIDTH_LS_KEY = 'schemaTableColumnsWidth';
+
+export const SCHEMA_TABLE_COLUMS_IDS = {
+    id: 'id',
+    name: 'name',
+    isKeyColumn: 'isKeyColumn',
+    type: 'type',
+    notNull: 'notNull',
+    familyName: 'familyName',
+    prefferedPoolKind: 'prefferedPoolKind',
+    columnCodec: 'columnCodec',
+} satisfies Record<string, keyof SchemaData>;
+
+const idColumn: SchemaColumn = {
+    name: SCHEMA_TABLE_COLUMS_IDS.id,
+    get header() {
+        return i18n('column-title.id');
+    },
+    width: 60,
+    render: ({row}) => row.id,
+};
+const nameColumn: SchemaColumn = {
+    name: SCHEMA_TABLE_COLUMS_IDS.name,
+    get header() {
+        return i18n('column-title.name');
+    },
+    width: 100,
+    render: ({row}) => row.name,
+};
+const keyColumn: SchemaColumn = {
+    name: SCHEMA_TABLE_COLUMS_IDS.isKeyColumn,
+    get header() {
+        return i18n('column-title.key');
+    },
+    width: 70,
+    resizeMinWidth: 70,
+    // Table should start with key columns on sort click
+    defaultOrder: DataTable.ASCENDING,
+    sortAccessor: (row) => row.keyAccessor,
+    render: ({row}) => {
+        return row.isKeyColumn ? (
+            <div className={b('key-icon')}>
+                <Icon data={keyIcon} width={12} height={7} />
+            </div>
+        ) : null;
+    },
+};
+const typeColumn: SchemaColumn = {
+    name: SCHEMA_TABLE_COLUMS_IDS.type,
+    get header() {
+        return i18n('column-title.type');
+    },
+    width: 100,
+    render: ({row}) => row.type,
+};
+const notNullColumn: SchemaColumn = {
+    name: SCHEMA_TABLE_COLUMS_IDS.notNull,
+    get header() {
+        return i18n('column-title.notNull');
+    },
+    width: 100,
+    // Table should start with notNull columns on sort click
+    defaultOrder: DataTable.DESCENDING,
+    render: ({row}) => {
+        if (row.notNull) {
+            return '\u2713';
+        }
+
+        return undefined;
+    },
+};
+const familyColumn: SchemaColumn = {
+    name: SCHEMA_TABLE_COLUMS_IDS.familyName,
+    get header() {
+        return i18n('column-title.family');
+    },
+    width: 100,
+    render: ({row}) => row.familyName,
+};
+const mediaColumn: SchemaColumn = {
+    name: SCHEMA_TABLE_COLUMS_IDS.prefferedPoolKind,
+    get header() {
+        return i18n('column-title.media');
+    },
+    width: 100,
+    render: ({row}) => row.prefferedPoolKind,
+};
+const compressionColumn: SchemaColumn = {
+    name: SCHEMA_TABLE_COLUMS_IDS.columnCodec,
+    get header() {
+        return i18n('column-title.compression');
+    },
+    width: 100,
+    render: ({row}) => row.columnCodec,
+};
+
+export function getViewColumns(): SchemaColumn[] {
+    return [nameColumn, typeColumn];
+}
+export function getExternalTableColumns(): SchemaColumn[] {
+    return [idColumn, nameColumn, typeColumn, notNullColumn];
+}
+export function getColumnTableColumns(): SchemaColumn[] {
+    return [idColumn, keyColumn, nameColumn, typeColumn, notNullColumn];
+}
+export function getRowTableColumns(extended: boolean): SchemaColumn[] {
+    const rowTableColumns = [idColumn, keyColumn, nameColumn, typeColumn, notNullColumn];
+
+    if (extended) {
+        return rowTableColumns.concat(familyColumn, mediaColumn, compressionColumn);
+    }
+
+    return rowTableColumns;
+}
