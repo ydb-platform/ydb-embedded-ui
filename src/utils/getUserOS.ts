42601bfadcd4df4d62778a603d35d4f082888c42
Ivan Samarin <adsidera@yandex-team.ru>
2024-06-26 01:24:06 +0300

    chore(utils): add utility to detect user os

---
+++ src/utils/getUserOS.ts
@@ -0,0 +1,37 @@
+interface GetUserOS {
+    (): 'mac' | 'windows' | 'linux' | 'unknown' | string;
+}
+
+export const getUserOS: GetUserOS = () => {
+    if (!globalThis.navigator) {
+        console.error(
+            'Navigator is missing in globalThis! It seems the function has called from non-browser',
+        );
+    }
+
+    const userAgent = globalThis.navigator.userAgent.toLowerCase();
+
+    if (userAgent.includes('macintosh')) {
+        return 'mac';
+    }
+
+    if (userAgent.includes('windows')) {
+        return 'windows';
+    }
+
+    if (userAgent.includes('linux')) {
+        return 'linux';
+    }
+
+    const [os] =
+        userAgent
+            .match(/\([.]+\)/)?.[0]
+            .replaceAll(/\(\);/, '')
+            .split('; ') || [];
+
+    if (os) {
+        return os.toLowerCase();
+    }
+
+    return 'unknown';
+};
