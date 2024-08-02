2f481628ebc9447056a1a5f92fd888eed2e6bd04
Valerii Sidorenko <balepas@nebius.com>
2024-08-02 17:42:57 +0200

    fix: add comment

--- src/utils/hooks/useEventHandler.ts
+++ src/utils/hooks/useEventHandler.ts
@@ -1,5 +1,10 @@
 import React from 'react';
 
+/**
+ * The hook returns a stable function (an empty list of dependencies),
+ * but this function always calls the actual function associated with the last render.
+ * The returned function should be used as an event handler or inside a useEffect.
+ */
 export function useEventHandler<T extends Function>(handler?: T) {
     const ref = React.useRef<T | undefined>(handler);
     React.useLayoutEffect(() => {
