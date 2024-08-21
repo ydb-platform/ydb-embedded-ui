603462d3ee1de811ee5e7b2d1d945a131d05966a
Anton Standrik <astandrik@gmail.com>
2024-08-21 14:51:54 +0300

    fix: add react-freeze as dep

--- src/components/NotRenderUntilFirstVisible/NotRenderUntilFirstVisible.tsx
+++ src/components/NotRenderUntilFirstVisible/NotRenderUntilFirstVisible.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import {cn} from '../../utils/cn';
+import {Freeze} from 'react-freeze';
 
-import {Freeze} from './Freeze';
+import {cn} from '../../utils/cn';
 
 import './NotRenderUntilFirstVisible.scss';
 
