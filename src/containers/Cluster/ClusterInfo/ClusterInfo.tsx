4eed8fe15d9081cbbddf26b2690daad997dc5dac
mufazalov <mufazalov@yandex-team.ru>
2024-08-22 16:33:16 +0300

    fix(AsyncReplicationInfo): do not show empty fields

--- src/containers/Cluster/ClusterInfo/ClusterInfo.tsx
+++ src/containers/Cluster/ClusterInfo/ClusterInfo.tsx
@@ -1,7 +1,7 @@
 import {ContentWithPopup} from '../../../components/ContentWithPopup/ContentWithPopup';
 import {ResponseError} from '../../../components/Errors/ResponseError';
 import type {InfoViewerItem} from '../../../components/InfoViewer/InfoViewer';
-import InfoViewer from '../../../components/InfoViewer/InfoViewer';
+import {InfoViewer} from '../../../components/InfoViewer/InfoViewer';
 import {InfoViewerSkeleton} from '../../../components/InfoViewerSkeleton/InfoViewerSkeleton';
 import {LinkWithIcon} from '../../../components/LinkWithIcon/LinkWithIcon';
 import {ProgressViewer} from '../../../components/ProgressViewer/ProgressViewer';
--- src/containers/Tenant/Diagnostics/Overview/AsyncReplicationInfo/AsyncReplicationInfo.tsx
+++ src/containers/Tenant/Diagnostics/Overview/AsyncReplicationInfo/AsyncReplicationInfo.tsx
@@ -1,7 +1,8 @@
+import type {DefinitionListItem} from '@gravity-ui/components';
 import {Flex, Text} from '@gravity-ui/uikit';
 
 import {AsyncReplicationState} from '../../../../../components/AsyncReplicationState';
-import {InfoViewer} from '../../../../../components/InfoViewer';
+import {YDBDefinitionList} from '../../../../../components/YDBDefinitionList/YDBDefinitionList';
 import type {TEvDescribeSchemeResult} from '../../../../../types/api/schema';
 import {getEntityName} from '../../../utils';
 import {AsyncReplicationPaths} from '../AsyncReplicationPaths';
@@ -25,55 +26,53 @@ export function AsyncReplicationInfo({data}: AsyncReplicationProps) {
         );
     }
 
+    const replicationItems = prepareReplicationItems(data);
+
     return (
         <Flex direction="column" gap="4">
-            <InfoViewer
-                title={entityName}
-                info={[
-                    {
-                        label: i18n('state.label'),
-                        value: (
-                            <AsyncReplicationState
-                                state={data.PathDescription?.ReplicationDescription?.State}
-                            />
-                        ),
-                    },
-                    {
-                        label: i18n('srcConnection.endpoint.label'),
-                        value: (
-                            <Text variant="code-inline-2">
-                                {
-                                    data.PathDescription?.ReplicationDescription?.Config
-                                        ?.SrcConnectionParams?.Endpoint
-                                }
-                            </Text>
-                        ),
-                    },
-                    {
-                        label: i18n('srcConnection.database.label'),
-                        value: (
-                            <Text variant="code-inline-2">
-                                {
-                                    data.PathDescription?.ReplicationDescription?.Config
-                                        ?.SrcConnectionParams?.Database
-                                }
-                            </Text>
-                        ),
-                    },
-                    {
-                        label: i18n('credentials.label'),
-                        value: (
-                            <Credentials
-                                connection={
-                                    data.PathDescription?.ReplicationDescription?.Config
-                                        ?.SrcConnectionParams
-                                }
-                            />
-                        ),
-                    },
-                ]}
-            />
+            <YDBDefinitionList title={entityName} items={replicationItems} />
             <AsyncReplicationPaths config={data.PathDescription?.ReplicationDescription?.Config} />
         </Flex>
     );
+}
+
+function prepareReplicationItems(data: TEvDescribeSchemeResult) {
+    const replicationDescription = data.PathDescription?.ReplicationDescription || {};
+    const state = replicationDescription.State;
+    const srcConnectionParams = replicationDescription.Config?.SrcConnectionParams || {};
+    const {Endpoint, Database} = srcConnectionParams;
+
+    const info: DefinitionListItem[] = [];
+
+    if (state) {
+        info.push({
+            name: i18n('state.label'),
+            content: <AsyncReplicationState state={state} />,
+        });
+    }
+
+    if (Endpoint) {
+        info.push({
+            name: i18n('srcConnection.endpoint.label'),
+            copyText: Endpoint,
+            content: <Text variant="code-inline-2">{Endpoint}</Text>,
+        });
+    }
+
+    if (Database) {
+        info.push({
+            name: i18n('srcConnection.database.label'),
+            copyText: Database,
+            content: <Text variant="code-inline-2">{Database}</Text>,
+        });
+    }
+
+    if (srcConnectionParams) {
+        info.push({
+            name: i18n('credentials.label'),
+            content: <Credentials connection={srcConnectionParams} />,
+        });
+    }
+
+    return info;
 }
