cd321942ae7e60ed5dcb46d23bc4bf5d04bb49a2
mufazalov <mufazalov@yandex-team.ru>
2024-12-05 22:28:06 +0300

    chore: remove unneeded nodes utils

--- src/store/reducers/nodes/selectors.ts
+++ src/store/reducers/nodes/selectors.ts
@@ -1,27 +1,8 @@
-import {EFlag} from '../../../types/api/enums';
 import {HOUR_IN_SECONDS} from '../../../utils/constants';
 import {calcUptimeInSeconds} from '../../../utils/dataFormatters/dataFormatters';
-import {prepareSearchValue} from '../../../utils/filters';
 import {NodesUptimeFilterValues} from '../../../utils/nodes';
-import {ProblemFilterValues} from '../settings/settings';
-import type {ProblemFilterValue} from '../settings/types';
-
-import type {NodesPreparedEntity} from './types';
 
 // ==== Filters ====
-
-const filterNodesByProblemsStatus = (
-    nodesList: NodesPreparedEntity[] = [],
-    problemFilter: ProblemFilterValue,
-) => {
-    if (problemFilter === ProblemFilterValues.ALL) {
-        return nodesList;
-    }
-
-    return nodesList.filter(({SystemState}) => {
-        return SystemState && SystemState !== EFlag.Green;
-    });
-};
 
 export const filterNodesByUptime = <T extends {StartTime?: string}>(
     nodesList: T[] = [],
@@ -34,33 +15,3 @@ export const filterNodesByUptime = <T extends {StartTime?: string}>(
         return !StartTime || calcUptimeInSeconds(StartTime) < HOUR_IN_SECONDS;
     });
 };
-
-const filterNodesBySearchValue = (nodesList: NodesPreparedEntity[] = [], searchValue: string) => {
-    if (!searchValue) {
-        return nodesList;
-    }
-    const re = prepareSearchValue(searchValue);
-
-    return nodesList.filter((node) => {
-        return node.Host ? re.test(node.Host) || re.test(String(node.NodeId)) : true;
-    });
-};
-
-export function filterNodes(
-    nodesList: NodesPreparedEntity[] = [],
-    {
-        uptimeFilter,
-        searchValue,
-        problemFilter,
-    }: {
-        uptimeFilter: NodesUptimeFilterValues;
-        searchValue: string;
-        problemFilter: ProblemFilterValue;
-    },
-) {
-    let result = filterNodesByUptime(nodesList, uptimeFilter);
-    result = filterNodesByProblemsStatus(result, problemFilter);
-    result = filterNodesBySearchValue(result, searchValue);
-
-    return result;
-}
