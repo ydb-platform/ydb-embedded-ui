6abc16e5d31024884628c56d32704cb8daa354d7
Артём Муфазалов <67755036+artemmufazalov@users.noreply.github.com>
2024-12-17 11:58:49 +0300

    feat: remove /meta/cluster (#1757)

--- src/services/parsers/parseMetaCluster.ts
+++ src/services/parsers/parseMetaCluster.ts
@@ -1,33 +1,4 @@
-import type {TClusterInfo} from '../../types/api/cluster';
-import type {MetaCluster} from '../../types/api/meta';
 import {traceCheckSchema, traceViewSchema} from '../../types/api/trace';
-
-export const parseMetaCluster = (data: MetaCluster): TClusterInfo => {
-    const {cluster = {}} = data;
-
-    const {
-        cluster: generalClusterInfo,
-        balancer,
-        solomon,
-        trace_check: traceCheck,
-        trace_view: traceView,
-    } = cluster;
-
-    const {traceCheck: TraceCheck, traceView: TraceView} = parseTraceFields({
-        traceCheck,
-        traceView,
-    });
-
-    return {
-        ...generalClusterInfo,
-        Name: cluster.title || generalClusterInfo?.Name,
-
-        Balancer: balancer,
-        Solomon: solomon,
-        TraceCheck,
-        TraceView,
-    };
-};
 
 export function parseTraceFields({
     traceCheck,
