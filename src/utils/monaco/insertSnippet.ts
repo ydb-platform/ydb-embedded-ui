1e296666c6d3f38819f89784bb5e489124f90afe
Hellen <el-makarova@yandex-team.ru>
2024-12-19 12:41:07 +0300

    fix(SchemaTree): snippet not insert if user is not on Query tab (#1766)

--- src/utils/monaco/insertSnippet.ts
+++ src/utils/monaco/insertSnippet.ts
@@ -1,6 +1,25 @@
-export function insertSnippetToEditor(input: string) {
-    if (!window.ydbEditor) {
+export async function insertSnippetToEditor(input: string) {
+    let retry = 1;
+
+    const checkEditor = async () => {
+        if (!window.ydbEditor) {
+            if (retry) {
+                await new Promise((r) => {
+                    window.setTimeout(r, 100);
+                });
+                retry -= 1;
+                checkEditor();
+            } else {
+                return false;
+            }
+        }
+        return true;
+    };
+    const editor = await checkEditor();
+    if (!editor) {
         console.error('Monaco editor not found');
+        return;
     }
+
     window.ydbEditor?.trigger(undefined, 'insertSnippetToEditor', input);
 }
