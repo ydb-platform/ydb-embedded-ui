f722c256dc2a57ab94d8a18f984be0527217372e
mufazalov <mufazalov@yandex-team.ru>
2025-03-12 18:15:03 +0300

    feat: request clusters handler only on Versions tab

--- src/containers/Versions/utils.ts
+++ src/containers/Versions/utils.ts
@@ -1,12 +1,20 @@
 import React from 'react';
 
 import {skipToken} from '@reduxjs/toolkit/query';
+import {StringParam, useQueryParam} from 'use-query-params';
 
+import {clustersApi} from '../../store/reducers/clusters/clusters';
 import {nodesApi} from '../../store/reducers/nodes/nodes';
 import {isClusterInfoV2} from '../../types/api/cluster';
 import type {TClusterInfo} from '../../types/api/cluster';
 import type {VersionToColorMap} from '../../types/versions';
-import {parseNodeGroupsToVersionsValues, parseNodesToVersionsValues} from '../../utils/versions';
+import {getVersionColors, getVersionMap} from '../../utils/clusterVersionColors';
+import {useTypedSelector} from '../../utils/hooks';
+import {
+    parseNodeGroupsToVersionsValues,
+    parseNodesToVersionsValues,
+    parseVersionsToVersionToColorMap,
+} from '../../utils/versions';
 
 export const useGetVersionValues = (cluster?: TClusterInfo, versionToColor?: VersionToColorMap) => {
     const {currentData} = nodesApi.useGetNodesQuery(
@@ -42,3 +50,33 @@ export const useGetVersionValues = (cluster?: TClusterInfo, versionToColor?: Ver
 
     return versionsValues;
 };
+
+export function useVersionToColorMap(cluster?: TClusterInfo) {
+    const getVersionToColorMap = useGetClusterVersionToColorMap();
+
+    return React.useMemo(() => {
+        if (getVersionToColorMap) {
+            return getVersionToColorMap();
+        }
+        return parseVersionsToVersionToColorMap(cluster?.Versions);
+    }, [cluster?.Versions, getVersionToColorMap]);
+}
+
+/** For multi-cluster version - with using meta handlers */
+function useGetClusterVersionToColorMap(): (() => VersionToColorMap) | undefined {
+    const [clusterName] = useQueryParam('clusterName', StringParam);
+    const singleClusterMode = useTypedSelector((state) => state.singleClusterMode);
+    const {data} = clustersApi.useGetClustersListQuery(undefined, {skip: singleClusterMode});
+
+    return React.useMemo(() => {
+        if (singleClusterMode) {
+            return undefined;
+        }
+
+        const clusters = data || [];
+        const info = clusters.find((cluster) => cluster.name === clusterName);
+        const versions = info?.versions || [];
+
+        return () => getVersionColors(getVersionMap(versions));
+    }, [singleClusterMode, data, clusterName]);
+}
