206ba88da620555df82e53a69ba6742df9dc1e84
Anton Standrik <astandrik@gmail.com>
2025-03-26 18:58:06 +0300

    feat: queries tab top design

--- src/components/TruncatedQuery/TruncatedQuery.tsx
+++ src/components/TruncatedQuery/TruncatedQuery.tsx
@@ -10,9 +10,16 @@ const b = cn('kv-truncated-query');
 interface TruncatedQueryProps {
     value?: string;
     maxQueryHeight?: number;
+    hasClipboardButton?: boolean;
+    clipboardButtonAlwaysVisible?: boolean;
 }
 
-export const TruncatedQuery = ({value = '', maxQueryHeight = 6}: TruncatedQueryProps) => {
+export const TruncatedQuery = ({
+    value = '',
+    maxQueryHeight = 6,
+    hasClipboardButton,
+    clipboardButtonAlwaysVisible,
+}: TruncatedQueryProps) => {
     const lines = value.split('\n');
     const truncated = lines.length > maxQueryHeight;
 
@@ -22,10 +29,37 @@ export const TruncatedQuery = ({value = '', maxQueryHeight = 6}: TruncatedQueryP
             '\n...\nThe request was truncated. Click on the line to show the full query on the query tab';
         return (
             <React.Fragment>
-                <YDBSyntaxHighlighter language="yql" className={b()} text={content} />
+                <YDBSyntaxHighlighter
+                    language="yql"
+                    className={b()}
+                    text={content}
+                    withClipboardButton={
+                        hasClipboardButton
+                            ? {
+                                  alwaysVisible: clipboardButtonAlwaysVisible,
+                                  copyText: value,
+                                  withLabel: false,
+                              }
+                            : false
+                    }
+                />
                 <span className={b('message', {color: 'secondary'})}>{message}</span>
             </React.Fragment>
         );
     }
-    return <YDBSyntaxHighlighter language="yql" text={value} />;
+    return (
+        <YDBSyntaxHighlighter
+            language="yql"
+            text={value}
+            withClipboardButton={
+                hasClipboardButton
+                    ? {
+                          alwaysVisible: clipboardButtonAlwaysVisible,
+                          copyText: value,
+                          withLabel: false,
+                      }
+                    : false
+            }
+        />
+    );
 };
