dfd810abe6beed330fbd814876e6267be670efb1
Anton Standrik <astandrik@yandex-team.ru>
2025-05-28 18:45:10 +0300

    Revert "Revert "Revert "Revert "Revert "Revert "fix: remove mocks""""""
    This reverts commit 843ec953bb0679d5762260425604008cdce984f6.

--- src/containers/Storage/PaginatedStorageNodesTable/getNodes.ts
+++ src/containers/Storage/PaginatedStorageNodesTable/getNodes.ts
@@ -13,8 +13,6 @@ import {prepareSortValue} from '../../../utils/filters';
 import {getUptimeParamValue} from '../../../utils/nodes';
 import {getRequiredDataFields} from '../../../utils/tableUtils/getRequiredDataFields';
 
-import {generateNodes} from './nodes';
-
 export const getStorageNodes: FetchData<
     PreparedStorageNode,
     PreparedStorageNodeFilters,
@@ -45,37 +43,23 @@ export const getStorageNodes: FetchData<
     const sort = sortField ? prepareSortValue(sortField, sortOrder) : undefined;
 
     const dataFieldsRequired = getRequiredDataFields(columnsIds, NODES_COLUMNS_TO_DATA_FIELDS);
-    let response;
-    const urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.get('mocks')) {
-        // Get mock configuration from URL parameters or use defaults
-        const pdisks = parseInt(urlParams.get('pdisks') || '10', 10);
-        const vdisksPerPDisk = parseInt(urlParams.get('vdisksPerPDisk') || '2', 10);
-        const totalNodes = parseInt(urlParams.get('totalNodes') || '50', 10);
-        response = generateNodes(totalNodes, {
-            maxVdisksPerPDisk: vdisksPerPDisk,
-            maxPdisks: pdisks,
-            offset,
-            limit,
-        });
-    } else {
-        response = await window.api.viewer.getNodes({
-            type,
-            storage,
-            limit,
-            offset,
-            sort,
-            filter: searchValue,
-            uptime: getUptimeParamValue(nodesUptimeFilter),
-            with: visibleEntities,
-            database,
-            node_id: nodeId,
-            group_id: groupId,
-            filter_group: filterGroup,
-            filter_group_by: filterGroupBy,
-            fieldsRequired: dataFieldsRequired,
-        });
-    }
+
+    const response = await window.api.viewer.getNodes({
+        type,
+        storage,
+        limit,
+        offset,
+        sort,
+        filter: searchValue,
+        uptime: getUptimeParamValue(nodesUptimeFilter),
+        with: visibleEntities,
+        database,
+        node_id: nodeId,
+        group_id: groupId,
+        filter_group: filterGroup,
+        filter_group_by: filterGroupBy,
+        fieldsRequired: dataFieldsRequired,
+    });
     const preparedResponse = prepareStorageNodesResponse(response);
     return {
         data: preparedResponse.nodes || [],
