f876431fe01a8405dfba1fce78a922dc850da803
Elena Makarova <el-makarova@yandex-team.ru>
2025-06-17 14:05:56 +0300

    fix: rebase

--- src/containers/Tenant/TenantDrawerWrappers.tsx
+++ src/containers/Tenant/TenantDrawerWrappers.tsx
@@ -9,30 +9,7 @@ interface TenantDrawerWrapperProps {
     children: React.ReactNode;
 }
 
-export function TenantDrawerWrapper({children, database}: TenantDrawerWrapperProps) {
-    const {
-        handleShowHealthcheckChange,
-        showHealthcheck,
-        handleIssuesFilterChange,
-        handleHealthcheckViewChange,
-    } = useTenantQueryParams();
-
-    const healthcheckStatus = useTypedSelector((state) => selectCheckStatus(state, database || ''));
-
-    const healthcheckData = useTypedSelector((state) =>
-        selectAllHealthcheckInfo(state, database || ''),
-    );
-
-    const handleCloseDrawer = React.useCallback(() => {
-        handleShowHealthcheckChange(false);
-        handleIssuesFilterChange(undefined);
-        handleHealthcheckViewChange(undefined);
-    }, [handleShowHealthcheckChange, handleIssuesFilterChange, handleHealthcheckViewChange]);
-
-    const renderDrawerContent = React.useCallback(() => {
-        return <Healthcheck tenantName={database} />;
-    }, [database]);
-
+export function TenantDrawerWrapper({children}: TenantDrawerWrapperProps) {
     return (
         <DrawerContextProvider>
             <TenantDrawerHealthcheck>
