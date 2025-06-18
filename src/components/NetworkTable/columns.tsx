9e07ba0c50eac57d00070d6a237bcd212395039a
mufazalov <mufazalov@yandex-team.ru>
2025-06-18 14:39:03 +0300

    feat(Cluster): add Network tab

---
+++ src/components/NetworkTable/columns.tsx
@@ -0,0 +1,41 @@
+import type {NodesPreparedEntity} from '../../store/reducers/nodes/types';
+import type {Column} from '../../utils/tableUtils/types';
+import {
+    getClockSkewColumn,
+    getConnectionsColumn,
+    getCpuColumn,
+    getDataCenterColumn,
+    getHostColumn,
+    getNetworkUtilizationColumn,
+    getNodeIdColumn,
+    getPingTimeColumn,
+    getPoolsColumn,
+    getRackColumn,
+    getReceiveThroughputColumn,
+    getSendThroughputColumn,
+    getUptimeColumn,
+} from '../nodesColumns/columns';
+import {isSortableNodesColumn} from '../nodesColumns/constants';
+import type {GetNodesColumnsParams} from '../nodesColumns/types';
+
+export function getNetworkTableNodesColumns(params: GetNodesColumnsParams) {
+    const columns: Column<NodesPreparedEntity>[] = [
+        getNodeIdColumn(),
+        getHostColumn(params, {statusForIcon: 'ConnectStatus'}),
+        getDataCenterColumn(),
+        getRackColumn(),
+        getUptimeColumn(),
+        getCpuColumn(),
+        getPoolsColumn(),
+        getConnectionsColumn(),
+        getNetworkUtilizationColumn(),
+        getSendThroughputColumn(),
+        getReceiveThroughputColumn(),
+        getPingTimeColumn(),
+        getClockSkewColumn(),
+    ];
+
+    return columns.map((column) => {
+        return {...column, sortable: isSortableNodesColumn(column.name)};
+    });
+}
