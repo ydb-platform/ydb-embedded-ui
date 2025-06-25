b53412e3299c70dd59a3c2782cb7e2e736f5b713
Anton Standrik <astandrik@yandex-team.ru>
2025-06-25 21:07:30 +0300

    fix: nanofix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,6 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function (app) {
     const metaYdbBackend = process.env.META_YDB_BACKEND;
-
     if (metaYdbBackend && metaYdbBackend !== 'undefined') {
         app.use(
             '/meta',
