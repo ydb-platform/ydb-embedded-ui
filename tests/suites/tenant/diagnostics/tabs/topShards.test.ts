8ac94d6366acc236b82bd018efe36bef7957d78b
astandrik <astandrik@yandex-team.ru>
2025-06-30 17:07:37 +0300

    fix: unskip tests

--- tests/suites/tenant/diagnostics/tabs/topShards.test.ts
+++ tests/suites/tenant/diagnostics/tabs/topShards.test.ts
@@ -89,10 +89,7 @@ test.describe('Diagnostics TopShards tab', async () => {
         }
     });
 
-    // TODO: https://github.com/ydb-platform/ydb-embedded-ui/issues/2459
-    test.skip('TopShards tab first row has values for all columns in History mode', async ({
-        page,
-    }) => {
+    test('TopShards tab first row has values for all columns in History mode', async ({page}) => {
         // Setup mock for TopShards tab in History mode
         await setupTopShardsHistoryMock(page);
 
