4d912d94119e18744eef10697a17101b56b6e8a8
Elena Makarova <el-makarova@yandex-team.ru>
2025-07-09 12:11:37 +0300

    fix: review

--- src/components/UsageLabel/UsageLabel.tsx
+++ src/components/UsageLabel/UsageLabel.tsx
@@ -3,7 +3,6 @@ import type {LabelProps} from '@gravity-ui/uikit';
 
 interface UsageLabelProps extends Omit<LabelProps, 'value'> {
     value: number | string;
-    overloadThreshold?: number;
 }
 
 export function UsageLabel({value, theme, ...props}: UsageLabelProps) {
