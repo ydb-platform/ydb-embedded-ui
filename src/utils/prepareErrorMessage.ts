a1b8e902feeba9f3b0be9e4a96a956e49a12ede6
Elena Makarova <el-makarova@yandex-team.ru>
2025-07-23 13:05:49 +0300

    feat(prepareErrorMessage): treat error.data as object

--- src/utils/prepareErrorMessage.ts
+++ src/utils/prepareErrorMessage.ts
@@ -4,8 +4,12 @@ export function prepareErrorMessage(error: unknown) {
     }
 
     if (error && typeof error === 'object') {
-        if ('data' in error && typeof error.data === 'string') {
-            return error.data;
+        if ('data' in error && error.data) {
+            if (typeof error.data === 'string') {
+                return error.data;
+            } else if (typeof error.data === 'object' && 'message' in error.data) {
+                return error.data.message;
+            }
         } else if ('statusText' in error && typeof error.statusText === 'string') {
             return error.statusText;
         } else if ('message' in error && typeof error.message === 'string') {
