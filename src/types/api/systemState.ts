661a8224c4bdf68f86a1bb46ac2bbba4d7ec841e
Elena Makarova <el-makarova@yandex-team.ru>
2025-08-05 13:08:23 +0300

    chore: fix types

--- src/types/api/systemState.ts
+++ src/types/api/systemState.ts
@@ -1,5 +1,4 @@
 import type {TSystemStateInfo} from './nodes';
-import type {TThreadPoolInfo} from './threads';
 
 /**
  * endpoint: /viewer/json/sysinfo
@@ -8,8 +7,6 @@ import type {TThreadPoolInfo} from './threads';
  */
 export interface TEvSystemStateResponse {
     SystemStateInfo?: TSystemStateInfo[];
-    /** Detailed thread information when fields_required=-1 is used */
-    Threads?: TThreadPoolInfo[];
     /** uint64 */
     ResponseTime?: string;
     ResponseDuration?: number;
