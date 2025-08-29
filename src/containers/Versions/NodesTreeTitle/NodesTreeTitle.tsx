d7d82fa1c81a4508027c13f2dd189b0ca73584ca
Артём Муфазалов <67755036+artemmufazalov@users.noreply.github.com>
2025-08-29 15:02:12 +0300

    fix(Versions): replace radio with sections (#2773)

--- src/containers/Versions/NodesTreeTitle/NodesTreeTitle.tsx
+++ src/containers/Versions/NodesTreeTitle/NodesTreeTitle.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import {ChevronDown, ChevronUp, Database} from '@gravity-ui/icons';
-import {ClipboardButton, Flex, Icon, Text} from '@gravity-ui/uikit';
+import {ArrowRight, ChevronDown, ChevronUp, Database} from '@gravity-ui/icons';
+import {Button, ClipboardButton, Flex, Icon, Text} from '@gravity-ui/uikit';
+import {useHistory} from 'react-router-dom';
 
 import {VersionsBar} from '../../../components/VersionsBar/VersionsBar';
 import {cn} from '../../../utils/cn';
 import type {PreparedNodeSystemState} from '../../../utils/nodes';
 import type {PreparedVersion} from '../../../utils/versions/types';
+import {getTenantPath} from '../../Tenant/TenantPages';
 import i18n from '../i18n';
 import type {GroupedNodesItem} from '../types';
 
@@ -35,6 +37,8 @@ export const NodesTreeTitle = ({
     preparedVersions,
     onClick,
 }: NodesTreeTitleProps) => {
+    const history = useHistory();
+
     const handleClick = React.useCallback<React.MouseEventHandler<HTMLDivElement>>(
         (event) => {
             const shouldSkip = event.nativeEvent.composedPath().some(isActiveButtonTarget);
@@ -59,6 +63,28 @@ export const NodesTreeTitle = ({
         }
     }, [items, nodes]);
 
+    const renderNodesCount = () => {
+        if (isDatabase) {
+            return (
+                <Button
+                    size="s"
+                    onClick={() =>
+                        history.push(getTenantPath({database: title, diagnosticsTab: 'nodes'}))
+                    }
+                >
+                    {i18n('nodes-count', {count: nodesAmount})}
+                    <Icon data={ArrowRight} />
+                </Button>
+            );
+        }
+
+        return (
+            <Text variant="body-2" color="hint">
+                {i18n('nodes-count', {count: nodesAmount})}
+            </Text>
+        );
+    };
+
     return (
         <div className={b('overview')} onClick={handleClick}>
             <Flex gap={2} alignItems={'center'}>
@@ -77,12 +103,10 @@ export const NodesTreeTitle = ({
                         />
                     </React.Fragment>
                 ) : null}
-                <Text variant="body-2" color="hint">
-                    {i18n('nodes-count', {count: nodesAmount})}
-                </Text>
+                {renderNodesCount()}
             </Flex>
             <Flex alignItems={'center'} gap={4}>
-                {preparedVersions ? (
+                {isDatabase && preparedVersions ? (
                     <div className={b('version-progress')}>
                         <VersionsBar preparedVersions={preparedVersions} withTitles={false} />
                     </div>
