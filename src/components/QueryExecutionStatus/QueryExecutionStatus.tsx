9e98f405d2f8e23070bac88b201a16b9415b88e3
Anton Standrik <astandrik@yandex-team.ru>
2025-09-09 18:28:35 +0300

    fix: write intelligent timing to query (#2853)
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- src/components/QueryExecutionStatus/QueryExecutionStatus.tsx
+++ src/components/QueryExecutionStatus/QueryExecutionStatus.tsx
@@ -12,6 +12,8 @@ import {HOUR_IN_SECONDS, SECOND_IN_MS} from '../../utils/constants';
 import {useTypedSelector} from '../../utils/hooks';
 import {isAxiosError} from '../../utils/response';
 
+import {useElapsedDuration} from './useElapsedDuration';
+
 import './QueryExecutionStatus.scss';
 
 const b = cn('kv-query-execution-status');
@@ -29,38 +31,17 @@ export const QueryExecutionStatus = ({className, error, loading}: QueryExecution
     let textColor: TextProps['color'];
     const {startTime, endTime} = useTypedSelector(selectQueryDuration);
 
-    const [queryDuration, setQueryDuration] = React.useState<number>(
-        startTime ? (endTime || Date.now()) - startTime : 0,
-    );
+    const durationMs = useElapsedDuration({startTime, endTime, loading});
+    const formattedDuration = React.useMemo(() => {
+        if (durationMs < 10 * SECOND_IN_MS) {
+            return duration(durationMs).format('mm:ss.SSS');
+        }
+        return durationMs > HOUR_IN_SECONDS * SECOND_IN_MS
+            ? duration(durationMs).format('hh:mm:ss')
+            : duration(durationMs).format('mm:ss');
+    }, [durationMs]);
 
     const isCancelled = isQueryCancelledError(error);
-
-    const setDuration = React.useCallback(() => {
-        if (startTime) {
-            const actualEndTime = endTime || Date.now();
-            setQueryDuration(actualEndTime - startTime);
-        }
-    }, [endTime, startTime]);
-
-    React.useEffect(() => {
-        let timerId: ReturnType<typeof setInterval> | undefined;
-        setDuration();
-
-        if (loading) {
-            timerId = setInterval(setDuration, SECOND_IN_MS);
-        } else {
-            clearInterval(timerId);
-        }
-        return () => {
-            clearInterval(timerId);
-        };
-    }, [loading, setDuration]);
-
-    const formattedQueryDuration = React.useMemo(() => {
-        return queryDuration > HOUR_IN_SECONDS * SECOND_IN_MS
-            ? duration(queryDuration).format('hh:mm:ss')
-            : duration(queryDuration).format('mm:ss');
-    }, [queryDuration]);
 
     if (loading) {
         theme = 'info';
@@ -96,7 +77,7 @@ export const QueryExecutionStatus = ({className, error, loading}: QueryExecution
             size="m"
             className={b(null, className)}
             icon={icon}
-            value={formattedQueryDuration}
+            value={formattedDuration}
         >
             <Text color={textColor}>{label}</Text>
         </Label>
