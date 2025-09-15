f76296e6d5e9929dd2c9551579d8af845198ad95
Elena Makarova <el-makarova@yandex-team.ru>
2025-09-15 16:25:45 +0300

    feat(TabletNameWrapper): remove popover

--- src/components/TabletNameWrapper/i18n/index.ts
+++
@@ -1,7 +0,0 @@
-import {registerKeysets} from '../../../utils/i18n';
-
-import en from './en.json';
-
-const COMPONENT = 'ydb-tablet-name-wrapper';
-
-export default registerKeysets(COMPONENT, {en});
