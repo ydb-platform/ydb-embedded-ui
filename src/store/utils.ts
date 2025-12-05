c25b134a1b6ed7e120b6abe4bcac14a3125ce089
mufazalov <mufazalov@yandex-team.ru>
2025-12-05 11:46:25 +0300

    update comment

--- src/store/utils.ts
+++ src/store/utils.ts
@@ -1,4 +1,8 @@
-/** Serialize the error to make it Redux-compatible */
+/**
+ * Serialize the error to make it Redux-compatible
+ *
+ * It prevents redux console error on string error in code - `throw new Error("description")`
+ */
 export function serializeError(error: unknown) {
     if (error instanceof Error) {
         return {message: error.message, name: error.name};
