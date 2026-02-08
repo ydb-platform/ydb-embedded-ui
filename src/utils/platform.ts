890d038f2a5c1aad88bf500522ab63561fa597de
Anton Standrik <astandrik@yandex-team.ru>
2026-02-08 20:52:04 +0300

    fix: supernanofix

--- src/utils/platform.ts
+++ src/utils/platform.ts
@@ -1,3 +1,10 @@
 export function isMacOS() {
-    return typeof navigator !== 'undefined' && navigator.platform.toUpperCase().includes('MAC');
+    if (typeof navigator === 'undefined') {
+        return false;
+    }
+
+    // navigator.userAgentData is not yet in TypeScript DOM types
+    const nav = navigator as Navigator & {userAgentData?: {platform: string}};
+    const platform = nav.userAgentData?.platform || navigator.platform;
+    return platform.toUpperCase().includes('MAC');
 }
