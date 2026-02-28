e8c41dd3092f4eed496716cc21973136a0e015e9
Arun Kumar <pheron408@gmail.com>
2026-02-28 23:17:21 +0530

    fix: use EMPTY_DATA_PLACEHOLDER instead of hardcoded “no data”

--- src/components/ProgressViewer/ProgressViewer.tsx
+++ src/components/ProgressViewer/ProgressViewer.tsx
@@ -1,5 +1,6 @@
 import {useTheme} from '@gravity-ui/uikit';
 
+import {EMPTY_DATA_PLACEHOLDER} from '../../lib';
 import {cn} from '../../utils/cn';
 import {calculateProgressStatus, defaultFormatProgressValues} from '../../utils/progress';
 import type {FormatProgressViewerValues} from '../../utils/progress';
@@ -103,5 +104,5 @@ export function ProgressViewer({
         );
     }
 
-    return <div className={`${b({size})} ${className} error`}>no data</div>;
+    return <div className={`${b({size})} ${className} error`}>{EMPTY_DATA_PLACEHOLDER}</div>;
 }
