219ed70d2851b6f61a242a8e5b31e937f2b834aa
Anton Standrik <astandrik@yandex-team.ru>
2026-03-03 15:41:57 +0300

    fix: workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ const config: PlaywrightTestConfig = {
               ['json', {outputFile: './playwright-artifacts/test-results.json'}],
           ],
     retries: process.env.CI ? 2 : 0,
-    workers: process.env.CI ? 1 : undefined,
+    workers: process.env.CI ? 2 : undefined,
     // If there is no url provided, playwright starts webServer with the app in dev mode
     webServer: baseUrl
         ? undefined
