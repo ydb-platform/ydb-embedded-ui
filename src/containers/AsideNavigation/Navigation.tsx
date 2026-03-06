407b269c6aabd9039362dd3c6d9747c9f851fc11
mufazalov <mufazalov@yandex-team.ru>
2026-03-06 16:09:06 +0300

    feat(Navigation): add colors and animation to v2 navigation items

--- src/containers/AsideNavigation/Navigation.tsx
+++ src/containers/AsideNavigation/Navigation.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import type {MenuItem} from '@gravity-ui/navigation';
 import {useRouteMatch} from 'react-router-dom';
 
 import {useComponent} from '../../components/ComponentsProvider/ComponentsProvider';
 import routes from '../../routes';
 import {selectUser} from '../../store/reducers/authentication/authentication';
+import {cn} from '../../utils/cn';
 import {useTypedSelector} from '../../utils/hooks';
 import {useTenantNavigation} from '../Tenant/TenantNavigation/useTenantNavigation';
 import {useNavigationV2Enabled} from '../Tenant/utils/useNavigationV2Enabled';
@@ -13,10 +15,15 @@ import type {YDBEmbeddedUISettings} from '../UserSettings/settings';
 
 import {YdbInternalUser} from './YdbInternalUser/YdbInternalUser';
 
+import './Navigation.scss';
+
+const b = cn('ydb-navigation-wrapper');
+
 interface NavigationProps {
     userSettings: YDBEmbeddedUISettings;
     children: React.ReactNode;
 }
+
 export function Navigation({children, userSettings}: NavigationProps) {
     const AsideNavigation = useComponent('AsideNavigation');
     const match = useRouteMatch(routes.tenant);
@@ -30,13 +37,47 @@ export function Navigation({children, userSettings}: NavigationProps) {
             return undefined;
         }
 
-        return tenantNavigationItems.map((item) => ({
-            id: item.id,
-            title: item.title,
-            icon: item.icon,
-            current: item.current,
-            onItemClick: item.onForward,
-        }));
+        return tenantNavigationItems.map((item) => {
+            const navigationItem: MenuItem = {
+                id: item.id,
+                title: item.title,
+                icon: item.icon,
+                current: item.current,
+                onItemClick: item.onForward,
+                tooltipText: item.title,
+                itemWrapper: (params, makeItem, options) => {
+                    const cnParams = {
+                        active: item.current,
+                        compact: options.compact,
+                        expanded: !options.compact,
+                        diagnostics: item.id === 'diagnostics',
+                        schema: item.id === 'schema',
+                        query: item.id === 'query',
+                        // TODO: animation should be shown for two weeks after a release
+                        // Probably should be sync with new navigation notification when it is added
+                        // https://github.com/ydb-platform/ydb-embedded-ui/issues/3587
+                        ['with-animation']: true,
+                    };
+
+                    // span1: full width wrapper to ensure proper button position
+                    // span2: wrapper with additional background and animation
+                    // span3: button wrapper for proper active and hover colors
+                    return (
+                        <span className={b('nav-item-wrapper')}>
+                            <span className={b('nav-item-bg', cnParams)}>
+                                <span className={b('button-wrapper', cnParams)}>
+                                    {makeItem({
+                                        ...params,
+                                    })}
+                                </span>
+                            </span>
+                        </span>
+                    );
+                },
+            };
+
+            return navigationItem;
+        });
     }, [match, isV2Enabled, tenantNavigationItems]);
 
     return (
@@ -46,6 +87,7 @@ export function Navigation({children, userSettings}: NavigationProps) {
             user={ydbUser ? {login: ydbUser} : undefined}
             menuItems={menuItems}
             content={children}
+            className={b()}
         />
     );
 }
