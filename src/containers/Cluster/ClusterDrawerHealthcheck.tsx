1d653bdcfa8f5f13f46a8feb06da98de36b25bc3
Alexey Efimov <xeno@ydb.tech>
2026-04-21 07:07:32 +0000

    refactor(ClusterHealthcheck): simplify healthcheck state management by removing database parameter

--- src/containers/Cluster/ClusterDrawerHealthcheck.tsx
+++ src/containers/Cluster/ClusterDrawerHealthcheck.tsx
@@ -26,20 +26,13 @@ interface ClusterDrawerHealthcheckProps {
 export function useClusterHealthcheckQueryParams() {
     const [{showHealthcheck, issuesFilter, view}, setQueryParams] = useQueryParams({
         showHealthcheck: BooleanParam,
-        database: StringParam,
         issuesFilter: StringParam,
         view: StringParam,
     });
 
     const handleShowHealthcheckChange = React.useCallback(
-        (value?: boolean, database?: string) => {
-            setQueryParams(
-                {
-                    showHealthcheck: value || undefined,
-                    database: value ? database : undefined,
-                },
-                'replaceIn',
-            );
+        (value?: boolean) => {
+            setQueryParams({showHealthcheck: value || undefined}, 'replaceIn');
         },
         [setQueryParams],
     );
@@ -81,7 +74,7 @@ export function ClusterDrawerHealthcheck({children, database}: ClusterDrawerHeal
     const healthcheckData = useTypedSelector((state) => selectAllHealthcheckInfo(state, database));
 
     const handleCloseDrawer = React.useCallback(() => {
-        handleShowHealthcheckChange(false, undefined);
+        handleShowHealthcheckChange(false);
         handleIssuesFilterChange(undefined);
         handleHealthcheckViewChange(undefined);
     }, [handleShowHealthcheckChange, handleIssuesFilterChange, handleHealthcheckViewChange]);
