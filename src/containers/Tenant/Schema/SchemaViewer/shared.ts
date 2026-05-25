a30c18e2317c72aa7af21759e374ad0658bfb4a9
Konstantin Kozlov <kozlovkon@yandex-team.ru>
2026-05-25 10:03:22 +0300

    feat: use i18n dictionary

--- src/containers/Tenant/Schema/SchemaViewer/shared.ts
+++ src/containers/Tenant/Schema/SchemaViewer/shared.ts
@@ -1,5 +1,7 @@
 import {cn} from '../../../../utils/cn';
 
+import i18n from './i18n';
+
 export const b = cn('schema-viewer');
 
-export const PLAIN_COLUMN_CODEC = 'Turned off';
+export const PLAIN_COLUMN_CODEC = i18n('column-codec.plain');
