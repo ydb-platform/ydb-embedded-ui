eb110a6e76efb841bf62a5156c4e996ea132f9aa
Anton Standrik <astandrik@yandex-team.ru>
2026-07-23 15:10:28 +0300

    test: remove topic message component test

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,8 +3,6 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import {TextDecoder, TextEncoder} from 'util';
-
 import '@testing-library/jest-dom';
 import {configure as configureUiKit} from '@gravity-ui/uikit';
 
@@ -12,14 +10,6 @@ import {Lang, i18n} from '../src/utils/i18n';
 
 i18n.setLang(Lang.En);
 configureUiKit({lang: Lang.En});
-
-// jsdom does not provide the Encoding API (used e.g. for decoding topic messages).
-if (typeof globalThis.TextEncoder === 'undefined') {
-    globalThis.TextEncoder = TextEncoder;
-}
-if (typeof globalThis.TextDecoder === 'undefined') {
-    globalThis.TextDecoder = TextDecoder;
-}
 
 // only to prevent warnings from history lib
 // all api calls in tests should be mocked
