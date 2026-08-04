dfd98e0bab9ba3f53534825017d4e6083337f64a
Elena Makarova <el-makarova@yandex-team.ru>
2026-08-04 11:38:45 +0300

    test(query-editor): keep issues test out of package

--- src/containers/Tenant/Query/Issues/__test__/Issues.test.tsx
+++ src/containers/Tenant/Query/Issues/__test__/Issues.test.tsx
@@ -1,6 +1,6 @@
 import {fireEvent, render, screen} from '@testing-library/react';
 
-import {ResultIssues} from './Issues';
+import {ResultIssues} from '../Issues';
 
 describe('ResultIssues', () => {
     beforeEach(() => {
