3d116e071f0f565d9bdf66f5051f5f4d35433bb9
Konstantin Kozlov <kozlovkon@yandex-team.ru>
2026-08-17 13:21:18 +0300

    fix(auth): honor oidc capabilities

--- src/store/reducers/capabilities/hooks.ts
+++ src/store/reducers/capabilities/hooks.ts
@@ -64,7 +64,7 @@ export const useOidcAvailable = () => {
     const authorizeAvailable = useGetMetaFeatureVersion('/meta/oidc/authorize') >= 1;
     const callbackAvailable = useGetMetaFeatureVersion('/meta/oidc/callback') >= 1;
 
-    return authorizeAvailable && callbackAvailable || true;
+    return authorizeAvailable && callbackAvailable;
 };
 
 export const useCreateDirectoryFeatureAvailable = () => {
